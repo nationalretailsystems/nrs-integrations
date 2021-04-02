4a0e7a01212d6d7c07256cf83048ea63b5a118c2
amagid <aaron@eradani.com>
2021-04-02 12:05:00 -0700

    Publish message to SNS (#1)

--- src/commands/pinc/controller.ts
+++ src/commands/pinc/controller.ts
@@ -1,13 +1,13 @@
 import { ECCHandlerFunction } from '@eradani-inc/ecc-router/types';
-import axios from 'axios';
 import { DateTime } from 'luxon';
+import AWS from 'aws-sdk';
 import config from 'config';
 import createLogger from 'src/services/logger';
 const logger = createLogger('commands/pinc');
 const { pinc } = config;
 import * as converter from 'src/interfaces/pncchkin';
-
-const axiosInstance = axios.create(pinc);
+// Set AWS region
+AWS.config.update(pinc.sns);
 
 export const checkin: ECCHandlerFunction = async (reqkey, data, ecc) => {
     // Get parameters from incomming data buffer
@@ -42,26 +42,27 @@ export const checkin: ECCHandlerFunction = async (reqkey, data, ecc) => {
     let result;
     let nextReqKey = reqkey;
     try {
-        // TODO: Replace this with Amazon SNS client.publish call
-        result = await axiosInstance.get('example', { params: reqFields });
-    } catch (err) {
-        if (err.response) {
-            // If the request was made and the server responded with a status code
-            // That falls out of the range of 2xx
-            // Note: These error formats are dependent on the web service
-            return ecc.sendEccResult('ECC1000', err.response.data.message, nextReqKey);
-        }
+        // Create publish parameters
+        const params = {
+            Message: JSON.stringify(reqFields),
+            MessageGroupId: 'nrs-test-1',
+            MessageDeduplicationId: '' + Date.now(),
+            TopicArn: pinc.sns.prdTargetArn
+        };
 
-        // Else the request was made but no response was received
-        // Note: This error format has nothing to do with the web service. This is
-        // Mainly TCP/IP errors.
+        // Create promise and SNS service object
+        result = await new AWS.SNS({ apiVersion: pinc.sns.apiVersion }).publish(params).promise();
+    } catch (err) {
         return ecc.sendEccResult('ECC1000', err.message, nextReqKey);
     }
-
-    // Send success result to client
-    if (!result.data.success) {
-        return ecc.sendEccResult('ECC1000', result.data.message, nextReqKey);
-    }
-
+    logger.debug(result);
     return ecc.sendEccResult('ECC0000', 'Success', nextReqKey);
 };
+
+checkin(
+    100000,
+    'aaaaaabbbbbbbbbbbbbbbbbbbbbbbbbcccccccccccccccccccccccccdddddddddddddddddddddddddeeeeeeeeeeeeeeeeeeeeeeeeeffffggggggggggggggggggggggggghhhhhhhhhhhhhhhhhhhhhhhhhiiiiiiiiiiiiiiiiiiiiiiiiijjjjjjjjjjjjjjjjjjjjjjjjjkkkkkkkkkkkkkkkkkkkkkkkkklllllllllllllllllllllllllmmmmmmmmmmmmmmmmmmmmmmmmm',
+    null as any,
+    converter,
+    {}
+);
