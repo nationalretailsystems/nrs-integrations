8dcc76a8c516e72d1fc2bb22878d1404f36c6c6d
David Romo <dave@eradani.com>
2021-07-01 18:28:01 -0700

    Adjust test checkin pgm for nested DSs

--- src/interfaces/pncchkin.ts
+++ src/interfaces/pncchkin.ts
@@ -7,17 +7,91 @@ import { ibmiConversions } from '@eradani-inc/ec-client';
 const { fromIbmiDate, fromIbmiTime, fromIbmiTimestamp, toIbmiDate, toIbmiTime, toIbmiTimestamp } = ibmiConversions;
 
 /**
+ * Data structure
+ */
+let DataDSFields: any = [
+    {
+        name: 'asset',
+        type: new dataTypes.DataStructure(AssetDSFields)
+    }
+];
+
+/**
+ * Input interface
+ */
+export interface DataDSInput {
+    /**
+     */
+    asset: AssetDSInput;
+}
+
+/**
  * Output interface
  */
-export interface CheckinDS {
+export interface DataDS {
     /**
-     * @size 25 characters
      */
-    message_group_id: string;
-    /**
-     * @size 6 characters
-     */
-    campus: string;
+    asset: AssetDS;
+}
+
+/**
+ * Data structure
+ */
+let AssetDSFields: any = [
+    {
+        name: 'checked_in',
+        type: new dataTypes.Char(25)
+    },
+    {
+        name: 'asset_type',
+        type: new dataTypes.Char(25)
+    },
+    {
+        name: 'rfid_tag',
+        type: new dataTypes.Char(25)
+    },
+    {
+        name: 'site_code',
+        type: new dataTypes.Char(25)
+    },
+    {
+        name: 'Trailer_SCAC',
+        type: new dataTypes.Char(4)
+    },
+    {
+        name: 'Trailer_number',
+        type: new dataTypes.Char(25)
+    },
+    {
+        name: 'container_number',
+        type: new dataTypes.Char(25)
+    },
+    {
+        name: 'movement_type',
+        type: new dataTypes.Char(25)
+    },
+    {
+        name: 'load_status',
+        type: new dataTypes.Char(25)
+    },
+    {
+        name: 'customer_code',
+        type: new dataTypes.Char(25)
+    },
+    {
+        name: 'fleet_code',
+        type: new dataTypes.Char(25)
+    },
+    {
+        name: 'tractor_scac',
+        type: new dataTypes.Char(25)
+    }
+];
+
+/**
+ * Input interface
+ */
+export interface AssetDSInput {
     /**
      * @size 25 characters
      */
@@ -69,6 +143,77 @@ export interface CheckinDS {
 }
 
 /**
+ * Output interface
+ */
+export interface AssetDS {
+    /**
+     * @size 25 characters
+     */
+    checked_in: string;
+    /**
+     * @size 25 characters
+     */
+    asset_type: string;
+    /**
+     * @size 25 characters
+     */
+    rfid_tag: string;
+    /**
+     * @size 25 characters
+     */
+    site_code: string;
+    /**
+     * @size 4 characters
+     */
+    Trailer_SCAC: string;
+    /**
+     * @size 25 characters
+     */
+    Trailer_number: string;
+    /**
+     * @size 25 characters
+     */
+    container_number: string;
+    /**
+     * @size 25 characters
+     */
+    movement_type: string;
+    /**
+     * @size 25 characters
+     */
+    load_status: string;
+    /**
+     * @size 25 characters
+     */
+    customer_code: string;
+    /**
+     * @size 25 characters
+     */
+    fleet_code: string;
+    /**
+     * @size 25 characters
+     */
+    tractor_scac: string;
+}
+
+/**
+ * Output interface
+ */
+export interface CheckinDS {
+    /**
+     * @size 25 characters
+     */
+    message_group_id: string;
+    /**
+     * @size 6 characters
+     */
+    campus: string;
+    /**
+     */
+    data: DataDS;
+}
+
+/**
  * Convert CheckinDS record to TypeScript object
  */
 export function convertCheckinDSToObject(dataIn: string): CheckinDS {
@@ -79,29 +224,31 @@ export function convertCheckinDSToObject(dataIn: string): CheckinDS {
     pos += 25;
     dataOut.campus = dataIn.substring(pos, pos + 6).trimEnd();
     pos += 6;
-    dataOut.checked_in = dataIn.substring(pos, pos + 25).trimEnd();
+    dataOut.data = {};
+    dataOut.data.asset = {};
+    dataOut.data.asset.checked_in = dataIn.substring(pos, pos + 25).trimEnd();
     pos += 25;
-    dataOut.asset_type = dataIn.substring(pos, pos + 25).trimEnd();
+    dataOut.data.asset.asset_type = dataIn.substring(pos, pos + 25).trimEnd();
     pos += 25;
-    dataOut.rfid_tag = dataIn.substring(pos, pos + 25).trimEnd();
+    dataOut.data.asset.rfid_tag = dataIn.substring(pos, pos + 25).trimEnd();
     pos += 25;
-    dataOut.site_code = dataIn.substring(pos, pos + 25).trimEnd();
+    dataOut.data.asset.site_code = dataIn.substring(pos, pos + 25).trimEnd();
     pos += 25;
-    dataOut.Trailer_SCAC = dataIn.substring(pos, pos + 4).trimEnd();
+    dataOut.data.asset.Trailer_SCAC = dataIn.substring(pos, pos + 4).trimEnd();
     pos += 4;
-    dataOut.Trailer_number = dataIn.substring(pos, pos + 25).trimEnd();
+    dataOut.data.asset.Trailer_number = dataIn.substring(pos, pos + 25).trimEnd();
     pos += 25;
-    dataOut.container_number = dataIn.substring(pos, pos + 25).trimEnd();
+    dataOut.data.asset.container_number = dataIn.substring(pos, pos + 25).trimEnd();
     pos += 25;
-    dataOut.movement_type = dataIn.substring(pos, pos + 25).trimEnd();
+    dataOut.data.asset.movement_type = dataIn.substring(pos, pos + 25).trimEnd();
     pos += 25;
-    dataOut.load_status = dataIn.substring(pos, pos + 25).trimEnd();
+    dataOut.data.asset.load_status = dataIn.substring(pos, pos + 25).trimEnd();
     pos += 25;
-    dataOut.customer_code = dataIn.substring(pos, pos + 25).trimEnd();
+    dataOut.data.asset.customer_code = dataIn.substring(pos, pos + 25).trimEnd();
     pos += 25;
-    dataOut.fleet_code = dataIn.substring(pos, pos + 25).trimEnd();
+    dataOut.data.asset.fleet_code = dataIn.substring(pos, pos + 25).trimEnd();
     pos += 25;
-    dataOut.tractor_scac = dataIn.substring(pos, pos + 25).trimEnd();
+    dataOut.data.asset.tractor_scac = dataIn.substring(pos, pos + 25).trimEnd();
     pos += 25;
 
     return dataOut;
