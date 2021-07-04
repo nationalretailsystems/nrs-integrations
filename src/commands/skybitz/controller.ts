46d04bdb9e49c94959b8db515a8a9e7690a70e85
amagid <aaron@eradani.com>
2021-07-04 11:26:11 -0400

    Change ECCResult codes (#7)     - ECC0000 -> Success     - ECC8100 -> API call failed (non-200)     - ECC9100 -> TCP/IP Error     - ECC9200 -> File Write Failed

--- src/commands/skybitz/controller.ts
+++ src/commands/skybitz/controller.ts
@@ -31,19 +31,19 @@ export const getTrailerMileages: ECCHandlerFunction = async (reqkey, data, ecc) 
             // If the request was made and the server responded with a status code
             // That falls out of the range of 2xx
             // Note: These error formats are dependent on the web service
-            return ecc.sendEccResult('ECC1000', err.response.status + '-' + err.response.statusText, nextReqKey);
+            return ecc.sendEccResult('ECC8100', err.response.status + '-' + err.response.statusText, nextReqKey);
         }
 
         // Else the request was made but no response was received
         // Note: This error format has nothing to do with the web service. This is
         // Mainly TCP/IP errors.
-        return ecc.sendEccResult('ECC1000', err.message, nextReqKey);
+        return ecc.sendEccResult('ECC9100', err.message, nextReqKey);
     }
 
     try {
         await fs.writeFile(reqFields.filename, result.data, 'utf-8');
     } catch (err) {
-        return ecc.sendEccResult('ECC1001', err.message, nextReqKey);
+        return ecc.sendEccResult('ECC9200', err.message, nextReqKey);
     }
 
     // Send the result info
