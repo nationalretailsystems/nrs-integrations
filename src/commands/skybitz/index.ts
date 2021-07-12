9448dbc0fa8841471e7ce79d95d78ce3a7961e6c
mcorbo <mikecorbo@verizon.net>
2021-07-12 12:01:43 -0400

    Added new Skybitz date mileage  api

--- src/commands/skybitz/index.ts
+++ src/commands/skybitz/index.ts
@@ -1,7 +1,8 @@
 import { ECCInternalRouter } from '@eradani-inc/ecc-router/ecc-router';
-import { getTrailerMileages } from './controller';
+import { getQueryMileage, getTrailerMileages } from './controller';
 import * as skybitz from 'src/interfaces/skybtz';
 
 export default function registerSkybitz(router: ECCInternalRouter) {
     router.use('trailermileages', skybitz, getTrailerMileages);
+    router.use('querymileage', skybitz, getQueryMileage);
 }
---
+++ src/interfaces/skybtzqm.ts
@@ -0,0 +1,32 @@
+/* eslint-disable */
+// @ts-nocheck
+// Module: skybtzqm
+// Generated source -- do not modify
+
+import { ibmiConversions } from '@eradani-inc/ec-client';
+const { fromIbmiDate, fromIbmiTime, fromIbmiTimestamp, toIbmiDate, toIbmiTime, toIbmiTimestamp } = ibmiConversions;
+
+/**
+ * Output interface
+ */
+export interface GetTrlMls {
+    /**
+     * @size 256 characters
+     */
+    filename: string;
+}
+
+/**
+ * Convert GetTrlMls record to TypeScript object
+ */
+export function convertGetTrlMlsToObject(dataIn: string): GetTrlMls {
+    const dataOut: any = {};
+    let pos: number = 0;
+
+    dataOut.filename = dataIn.substring(pos, pos + 256).trimEnd();
+    pos += 256;
+
+    return dataOut;
+}
+
+/* eslint-enable */
