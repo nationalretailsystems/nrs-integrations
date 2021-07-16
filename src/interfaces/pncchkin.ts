b025b47eff95248b1fe6a8f0dd03b9fbcd31895f
mcorbo <mikecorbo@verizon.net>
2021-07-16 11:34:11 -0400

    updated

--- src/interfaces/pncchkin.ts
+++ src/interfaces/pncchkin.ts
@@ -3,72 +3,65 @@
 // Module: pncchkin
 // Generated source -- do not modify
 
-import { ibmiConversions } from "@eradani-inc/ec-client";
-const {
-  fromIbmiDate,
-  fromIbmiTime,
-  fromIbmiTimestamp,
-  toIbmiDate,
-  toIbmiTime,
-  toIbmiTimestamp
-} = ibmiConversions;
+import { ibmiConversions } from '@eradani-inc/ec-client';
+const { fromIbmiDate, fromIbmiTime, fromIbmiTimestamp, toIbmiDate, toIbmiTime, toIbmiTimestamp } = ibmiConversions;
 
-import eradaniConnect from "@eradani-inc/eradani-connect";
+import eradaniConnect from '@eradani-inc/eradani-connect';
 const { dataTypes } = eradaniConnect;
 
 /**
  * Data structure
  */
-  let AssetDSFields: any = [
-      {
-      name: "checked_in",
-      type: new dataTypes.Char(25)
+let AssetDSFields: any = [
+    {
+        name: 'checked_in',
+        type: new dataTypes.Char(25)
     },
-      {
-      name: "asset_type",
-      type: new dataTypes.Char(25)
+    {
+        name: 'asset_type',
+        type: new dataTypes.Char(25)
     },
-      {
-      name: "rfid_tag",
-      type: new dataTypes.Char(25)
+    {
+        name: 'rfid_tag',
+        type: new dataTypes.Char(25)
     },
-      {
-      name: "site_code",
-      type: new dataTypes.Char(25)
+    {
+        name: 'site_code',
+        type: new dataTypes.Char(25)
     },
-      {
-      name: "Trailer_SCAC",
-      type: new dataTypes.Char(4)
+    {
+        name: 'Trailer_SCAC',
+        type: new dataTypes.Char(4)
     },
-      {
-      name: "Trailer_number",
-      type: new dataTypes.Char(25)
+    {
+        name: 'Trailer_number',
+        type: new dataTypes.Char(25)
     },
-      {
-      name: "container_number",
-      type: new dataTypes.Char(25)
+    {
+        name: 'container_number',
+        type: new dataTypes.Char(25)
     },
-      {
-      name: "movement_type",
-      type: new dataTypes.Char(25)
+    {
+        name: 'movement_type',
+        type: new dataTypes.Char(25)
     },
-      {
-      name: "load_status",
-      type: new dataTypes.Char(25)
+    {
+        name: 'load_status',
+        type: new dataTypes.Char(25)
     },
-      {
-      name: "customer_code",
-      type: new dataTypes.Char(25)
+    {
+        name: 'customer_code',
+        type: new dataTypes.Char(25)
     },
-      {
-      name: "fleet_code",
-      type: new dataTypes.Char(25)
+    {
+        name: 'fleet_code',
+        type: new dataTypes.Char(25)
     },
-      {
-      name: "tractor_scac",
-      type: new dataTypes.Char(25)
+    {
+        name: 'tractor_scac',
+        type: new dataTypes.Char(25)
     }
-  ];
+];
 
 /**
  * Input interface
@@ -77,51 +70,51 @@ export interface AssetDSInput {
     /**
      * @size 25 characters
      */
-    checked_in: string,
+    checked_in: string;
     /**
      * @size 25 characters
      */
-    asset_type: string,
+    asset_type: string;
     /**
      * @size 25 characters
      */
-    rfid_tag: string,
+    rfid_tag: string;
     /**
      * @size 25 characters
      */
-    site_code: string,
+    site_code: string;
     /**
      * @size 4 characters
      */
-    Trailer_SCAC: string,
+    Trailer_SCAC: string;
     /**
      * @size 25 characters
      */
-    Trailer_number: string,
+    Trailer_number: string;
     /**
      * @size 25 characters
      */
-    container_number: string,
+    container_number: string;
     /**
      * @size 25 characters
      */
-    movement_type: string,
+    movement_type: string;
     /**
      * @size 25 characters
      */
-    load_status: string,
+    load_status: string;
     /**
      * @size 25 characters
      */
-    customer_code: string,
+    customer_code: string;
     /**
      * @size 25 characters
      */
-    fleet_code: string,
+    fleet_code: string;
     /**
      * @size 25 characters
      */
-    tractor_scac: string
+    tractor_scac: string;
 }
 
 /**
@@ -131,62 +124,62 @@ export interface AssetDS {
     /**
      * @size 25 characters
      */
-    checked_in: string,
+    checked_in: string;
     /**
      * @size 25 characters
      */
-    asset_type: string,
+    asset_type: string;
     /**
      * @size 25 characters
      */
-    rfid_tag: string,
+    rfid_tag: string;
     /**
      * @size 25 characters
      */
-    site_code: string,
+    site_code: string;
     /**
      * @size 4 characters
      */
-    Trailer_SCAC: string,
+    Trailer_SCAC: string;
     /**
      * @size 25 characters
      */
-    Trailer_number: string,
+    Trailer_number: string;
     /**
      * @size 25 characters
      */
-    container_number: string,
+    container_number: string;
     /**
      * @size 25 characters
      */
-    movement_type: string,
+    movement_type: string;
     /**
      * @size 25 characters
      */
-    load_status: string,
+    load_status: string;
     /**
      * @size 25 characters
      */
-    customer_code: string,
+    customer_code: string;
     /**
      * @size 25 characters
      */
-    fleet_code: string,
+    fleet_code: string;
     /**
      * @size 25 characters
      */
-    tractor_scac: string
+    tractor_scac: string;
 }
 
 /**
  * Data structure
  */
-  let DataDSFields: any = [
-      {
-      name: "asset",
-      type: new dataTypes.DataStructure(AssetDSFields)
+let DataDSFields: any = [
+    {
+        name: 'asset',
+        type: new dataTypes.DataStructure(AssetDSFields)
     }
-  ];
+];
 
 /**
  * Input interface
@@ -194,7 +187,7 @@ export interface AssetDS {
 export interface DataDSInput {
     /**
      */
-    asset: AssetDSInput
+    asset: AssetDSInput;
 }
 
 /**
@@ -203,7 +196,7 @@ export interface DataDSInput {
 export interface DataDS {
     /**
      */
-    asset: AssetDS
+    asset: AssetDS;
 }
 
 /**
@@ -213,61 +206,55 @@ export interface CheckinDS {
     /**
      * @size 25 characters
      */
-    message_group_id: string,
+    message_group_id: string;
     /**
      * @size 6 characters
      */
-    campus: string,
+    campus: string;
     /**
      */
-    data: DataDS
+    data: DataDS;
 }
 
 /**
  * Convert CheckinDS record to TypeScript object
  */
 export function convertCheckinDSToObject(dataIn: string): CheckinDS {
-  const dataOut: any =   {
-  
-    };
-  let pos: number = 0;
+    const dataOut: any = {};
+    let pos: number = 0;
 
-  dataOut.message_group_id = dataIn.substring(pos, pos + 25).trimEnd();
-  pos += 25;
-  dataOut.campus = dataIn.substring(pos, pos + 6).trimEnd();
-  pos += 6;
-  dataOut.data =   {
-  
-    };
-  dataOut.data.asset =   {
-  
-    };
-  dataOut.data.asset.checked_in = dataIn.substring(pos, pos + 25).trimEnd();
-  pos += 25;
-  dataOut.data.asset.asset_type = dataIn.substring(pos, pos + 25).trimEnd();
-  pos += 25;
-  dataOut.data.asset.rfid_tag = dataIn.substring(pos, pos + 25).trimEnd();
-  pos += 25;
-  dataOut.data.asset.site_code = dataIn.substring(pos, pos + 25).trimEnd();
-  pos += 25;
-  dataOut.data.asset.Trailer_SCAC = dataIn.substring(pos, pos + 4).trimEnd();
-  pos += 4;
-  dataOut.data.asset.Trailer_number = dataIn.substring(pos, pos + 25).trimEnd();
-  pos += 25;
-  dataOut.data.asset.container_number = dataIn.substring(pos, pos + 25).trimEnd();
-  pos += 25;
-  dataOut.data.asset.movement_type = dataIn.substring(pos, pos + 25).trimEnd();
-  pos += 25;
-  dataOut.data.asset.load_status = dataIn.substring(pos, pos + 25).trimEnd();
-  pos += 25;
-  dataOut.data.asset.customer_code = dataIn.substring(pos, pos + 25).trimEnd();
-  pos += 25;
-  dataOut.data.asset.fleet_code = dataIn.substring(pos, pos + 25).trimEnd();
-  pos += 25;
-  dataOut.data.asset.tractor_scac = dataIn.substring(pos, pos + 25).trimEnd();
-  pos += 25;
+    dataOut.message_group_id = dataIn.substring(pos, pos + 25).trimEnd();
+    pos += 25;
+    dataOut.campus = dataIn.substring(pos, pos + 6).trimEnd();
+    pos += 6;
+    dataOut.data = {};
+    dataOut.data.asset = {};
+    dataOut.data.asset.checked_in = dataIn.substring(pos, pos + 25).trimEnd();
+    pos += 25;
+    dataOut.data.asset.asset_type = dataIn.substring(pos, pos + 25).trimEnd();
+    pos += 25;
+    dataOut.data.asset.rfid_tag = dataIn.substring(pos, pos + 25).trimEnd();
+    pos += 25;
+    dataOut.data.asset.site_code = dataIn.substring(pos, pos + 25).trimEnd();
+    pos += 25;
+    dataOut.data.asset.Trailer_SCAC = dataIn.substring(pos, pos + 4).trimEnd();
+    pos += 4;
+    dataOut.data.asset.Trailer_number = dataIn.substring(pos, pos + 25).trimEnd();
+    pos += 25;
+    dataOut.data.asset.container_number = dataIn.substring(pos, pos + 25).trimEnd();
+    pos += 25;
+    dataOut.data.asset.movement_type = dataIn.substring(pos, pos + 25).trimEnd();
+    pos += 25;
+    dataOut.data.asset.load_status = dataIn.substring(pos, pos + 25).trimEnd();
+    pos += 25;
+    dataOut.data.asset.customer_code = dataIn.substring(pos, pos + 25).trimEnd();
+    pos += 25;
+    dataOut.data.asset.fleet_code = dataIn.substring(pos, pos + 25).trimEnd();
+    pos += 25;
+    dataOut.data.asset.tractor_scac = dataIn.substring(pos, pos + 25).trimEnd();
+    pos += 25;
 
-  return dataOut;
+    return dataOut;
 }
 
 /* eslint-enable */
