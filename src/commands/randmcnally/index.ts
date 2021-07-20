06e33283ae6a41ac8fa892ad0a7d4f27167982d9
mcorbo <mikecorbo@verizon.net>
2021-07-20 10:02:36 -0400

    RM

--- src/commands/randmcnally/index.ts
+++ src/commands/randmcnally/index.ts
@@ -2,6 +2,6 @@ import { ECCInternalRouter } from '@eradani-inc/ecc-router/ecc-router';
 import { getStateMiles } from './controller';
 import * as randmcnally from 'src/interfaces/rmgetstmi';
 
-export default function registerMileageplus(router: ECCInternalRouter) {
+export default function registerRandMcnally(router: ECCInternalRouter) {
     router.use('getStateMiles', randmcnally, getStateMiles);
 }
