2b244948c466ed12dc2fcfa301294019b0f8f9f4
mcorbo <mikecorbo@verizon.net>
2021-09-23 13:36:11 -0400

    DVIR

--- src/interfaces/rmgetdvir.ts
+++ src/interfaces/rmgetdvir.ts
@@ -19,7 +19,73 @@ const { dataTypes } = eradaniConnect;
 /**
  * Output interface
  */
-export interface dvirDS {
+export interface responseDS {
+    /**
+     * @size 5 digits
+     */
+    code: number,
+    /**
+     * @size 10 characters
+     */
+    type: string,
+    /**
+     * @size 200 characters
+     */
+    message: string
+}
+
+/**
+ * Output interface
+ */
+export interface ReqDVIR {
+    /**
+     * @size 100 characters
+     */
+    accessToken: string,
+    /**
+     * @size 10 characters
+     */
+    companyCode: string,
+    /**
+     */
+    lastModifiedDate: Date,
+    /**
+     * @size 10 digits
+     */
+    startIndex: number,
+    /**
+     * @size 10 digits
+     */
+    size: number
+}
+
+/**
+ * Convert ReqDVIR record to TypeScript object
+ */
+export function convertReqDVIRToObject(dataIn: string): ReqDVIR {
+  const dataOut: any =   {
+  
+    };
+  let pos: number = 0;
+
+  dataOut.accessToken = dataIn.substring(pos, pos + 100).trimEnd();
+  pos += 100;
+  dataOut.companyCode = dataIn.substring(pos, pos + 10).trimEnd();
+  pos += 10;
+  dataOut.lastModifiedDate = fromIbmiDate(dataIn.substring(pos, pos + 10).trimEnd());
+  pos += 10;
+  dataOut.startIndex = Number(dataIn.substring(pos, pos + 11).trimEnd());
+  pos += 11;
+  dataOut.size = Number(dataIn.substring(pos, pos + 11).trimEnd());
+  pos += 11;
+
+  return dataOut;
+}
+
+/**
+ * Input interface
+ */
+export interface RtnDVIR {
     /**
      * @size 10 digits
      */
@@ -119,117 +185,42 @@ export interface dvirDS {
 }
 
 /**
- * Output interface
- */
-export interface responseDS {
-    /**
-     * @size 5 digits
-     */
-    code: number,
-    /**
-     * @size 10 characters
-     */
-    type: string,
-    /**
-     * @size 200 characters
-     */
-    message: string
-}
-
-/**
- * Output interface
- */
-export interface ReqDVIR {
-    /**
-     * @size 100 characters
-     */
-    accessToken: string,
-    /**
-     * @size 10 characters
-     */
-    companyCode: string,
-    /**
-     */
-    lastModifiedDate: Date,
-    /**
-     * @size 10 digits
-     */
-    startIndex: number,
-    /**
-     * @size 10 digits
-     */
-    size: number
-}
-
-/**
- * Convert ReqDVIR record to TypeScript object
- */
-export function convertReqDVIRToObject(dataIn: string): ReqDVIR {
-  const dataOut: any =   {
-  
-    };
-  let pos: number = 0;
-
-  dataOut.accessToken = dataIn.substring(pos, pos + 100).trimEnd();
-  pos += 100;
-  dataOut.companyCode = dataIn.substring(pos, pos + 10).trimEnd();
-  pos += 10;
-  dataOut.lastModifiedDate = fromIbmiDate(dataIn.substring(pos, pos + 10).trimEnd());
-  pos += 10;
-  dataOut.startIndex = Number(dataIn.substring(pos, pos + 11).trimEnd());
-  pos += 11;
-  dataOut.size = Number(dataIn.substring(pos, pos + 11).trimEnd());
-  pos += 11;
-
-  return dataOut;
-}
-
-/**
- * Input interface
- */
-export interface RtnDVIR {
-    /**
-     */
-    dvirs: dvirDS
-}
-
-/**
  * Convert JavaScript object to RtnDVIR record
  */
 export function convertObjectToRtnDVIR(dataIn: RtnDVIR): string {
   let dataOut: string = "";
 
-  dataOut += dataIn.dvirs.index.toFixed().substring(0, 11).padEnd(11);
-  dataOut += dataIn.dvirs.driverId.substring(0, 60).padEnd(60);
-  dataOut += dataIn.dvirs.createdTimeStamp.toFixed().substring(0, 21).padEnd(21);
-  dataOut += dataIn.dvirs.modifiedTimeStamp.toFixed().substring(0, 21).padEnd(21);
-  dataOut += dataIn.dvirs.timeZone.substring(0, 5).padEnd(5);
-  dataOut += dataIn.dvirs.tripName.substring(0, 200).padEnd(200);
-  dataOut += dataIn.dvirs.driverComment.substring(0, 200).padEnd(200);
-  dataOut += dataIn.dvirs.tractorNumber.substring(0, 16).padEnd(16);
-  dataOut += dataIn.dvirs.trailerNumber.substring(0, 32).padEnd(32);
-  dataOut += dataIn.dvirs.totalTractorDefects.toFixed().substring(0, 11).padEnd(11);
-  for (let j: number = 0; j < 25; ++j) {
-  dataOut += dataIn.dvirs.tractorDefects[j].substring(0, 50).padEnd(50);
+  dataOut += dataIn.index.toFixed().substring(0, 11).padEnd(11);
+  dataOut += dataIn.driverId.substring(0, 60).padEnd(60);
+  dataOut += dataIn.createdTimeStamp.toFixed().substring(0, 21).padEnd(21);
+  dataOut += dataIn.modifiedTimeStamp.toFixed().substring(0, 21).padEnd(21);
+  dataOut += dataIn.timeZone.substring(0, 5).padEnd(5);
+  dataOut += dataIn.tripName.substring(0, 200).padEnd(200);
+  dataOut += dataIn.driverComment.substring(0, 200).padEnd(200);
+  dataOut += dataIn.tractorNumber.substring(0, 16).padEnd(16);
+  dataOut += dataIn.trailerNumber.substring(0, 32).padEnd(32);
+  dataOut += dataIn.totalTractorDefects.toFixed().substring(0, 11).padEnd(11);
+  for (let i: number = 0; i < 25; ++i) {
+  dataOut += dataIn.tractorDefects[i].substring(0, 50).padEnd(50);
   }
-  dataOut += dataIn.dvirs.totalTrailerDefects.toFixed().substring(0, 11).padEnd(11);
-  for (let j: number = 0; j < 25; ++j) {
-  dataOut += dataIn.dvirs.trailerDefects[j].substring(0, 50).padEnd(50);
+  dataOut += dataIn.totalTrailerDefects.toFixed().substring(0, 11).padEnd(11);
+  for (let i: number = 0; i < 25; ++i) {
+  dataOut += dataIn.trailerDefects[i].substring(0, 50).padEnd(50);
   }
-  for (let j: number = 0; j < 10; ++j) {
-  dataOut += dataIn.dvirs.defectImages[j].substring(0, 255).padEnd(255);
+  for (let i: number = 0; i < 10; ++i) {
+  dataOut += dataIn.defectImages[i].substring(0, 255).padEnd(255);
   }
-  dataOut += (dataIn.dvirs.dvirIsDefective ? "1" : "0");
-  dataOut += (dataIn.dvirs.dvirIsCertified ? "1" : "0");
-  dataOut += dataIn.dvirs.driverSignature.substring(0, 200).padEnd(200);
-  dataOut += dataIn.dvirs.mechanicSignature.substring(0, 200).padEnd(200);
-  dataOut += dataIn.dvirs.latitude.toExponential(7).substring(0, 14).padEnd(14);
-  dataOut += dataIn.dvirs.longitude.toExponential(7).substring(0, 14).padEnd(14);
-  dataOut += dataIn.dvirs.location.substring(0, 200).padEnd(200);
-  dataOut += dataIn.dvirs.odometer.toExponential(7).substring(0, 14).padEnd(14);
-  dataOut += dataIn.dvirs.status.substring(0, 100).padEnd(100);
-  dataOut += toIbmiDate(dataIn.dvirs.lastModifiedData);
-  dataOut += (dataIn.dvirs.dvirIsDST ? "1" : "0");
+  dataOut += (dataIn.dvirIsDefective ? "1" : "0");
+  dataOut += (dataIn.dvirIsCertified ? "1" : "0");
+  dataOut += dataIn.driverSignature.substring(0, 200).padEnd(200);
+  dataOut += dataIn.mechanicSignature.substring(0, 200).padEnd(200);
+  dataOut += dataIn.latitude.toExponential(7).substring(0, 14).padEnd(14);
+  dataOut += dataIn.longitude.toExponential(7).substring(0, 14).padEnd(14);
+  dataOut += dataIn.location.substring(0, 200).padEnd(200);
+  dataOut += dataIn.odometer.toExponential(7).substring(0, 14).padEnd(14);
+  dataOut += dataIn.status.substring(0, 100).padEnd(100);
+  dataOut += toIbmiDate(dataIn.lastModifiedData);
+  dataOut += (dataIn.dvirIsDST ? "1" : "0");
 
   return dataOut;
 }
@@ -240,7 +231,7 @@ export function convertObjectToRtnDVIR(dataIn: RtnDVIR): string {
 export interface RtnDVIRRes {
     /**
      */
-    response: responseDS,
+    repsonse: responseDS,
     /**
      * @size 200 characters
      */
@@ -253,9 +244,9 @@ export interface RtnDVIRRes {
 export function convertObjectToRtnDVIRRes(dataIn: RtnDVIRRes): string {
   let dataOut: string = "";
 
-  dataOut += dataIn.response.code.toFixed().substring(0, 6).padEnd(6);
-  dataOut += dataIn.response.type.substring(0, 10).padEnd(10);
-  dataOut += dataIn.response.message.substring(0, 200).padEnd(200);
+  dataOut += dataIn.repsonse.code.toFixed().substring(0, 6).padEnd(6);
+  dataOut += dataIn.repsonse.type.substring(0, 10).padEnd(10);
+  dataOut += dataIn.repsonse.message.substring(0, 200).padEnd(200);
   dataOut += dataIn.imageURL.substring(0, 200).padEnd(200);
 
   return dataOut;
