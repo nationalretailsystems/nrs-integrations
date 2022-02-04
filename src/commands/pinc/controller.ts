1fd30adbe31a58f729510ab88aabaeb559a016be
mcorbo <mikecorbo@verizon.net>
2022-02-04 08:45:45 -0500

    nEW pINC cONROLLER

--- src/commands/pinc/controller.ts
+++ src/commands/pinc/controller.ts
@@ -16,15 +16,71 @@ import * as pncerrorapi from 'src/interfaces/pncerror';
 import transport from 'src/services/connection';
 import { insertPincSnsLog, insertPincSqsLog } from 'src/models/pinc';
 import { promises as fs } from 'fs';
+import { sanitizeValues } from 'src/services/safe-values';
 // Set AWS region
 AWS.config.update(pinc.sns);
 let sns = new AWS.SNS({ apiVersion: pinc.sns.apiVersion });
 let sqs = new AWS.SQS({ apiVersion: pinc.sqs.apiVersion });
+const safeValues: any = {
+    campus: '',
+    customer_facility_code: '',
+    event: '',
+    event_id: '',
+    time: '0000-00-00T00:00:00.000+00:00',
+    version: '',
+    data: {
+        asset: {
+            asset_dimension: '',
+            asset_type: '',
+            asset_visit_id: 0,
+            check_in_agent: '',
+            check_out_agent: '',
+            checked_in: '0000-00-00T00:00:00.000+00:00',
+            checked_out: '0000-00-00T00:00:00.000+00:00',
+            is_dock: '',
+            last_move_time: '0000-00-00T00:00:00.000+00:00',
+            Latitude: '',
+            Longitude: '',
+            purpose: '',
+            reference_id: '',
+            rfid_tag: '',
+            site: '',
+            site_code: '',
+            spot_number: '',
+            'Trailer #': '',
+            'Trailer SCAC': '',
+            trailer_number: '',
+            trailer_scac: '',
+            updated_on: '0000-00-00T00:00:00.000+00:00',
+            movement_type: '',
+            load_status: '',
+            container_number: '',
+            fleet_code: '',
+            tractor_scac: '',
+            customer_code: ''
+            }
+        }
+    }
+
+function getCampus(id: string) {
+    return {
+        NRSNBR: 'nbr',
+        NRSSAV: 'sav'
+    }[id];
+}
+function getCampusAws(id: string) {
+    const campus = getCampus(id);
+    return {
+        sqsUrl: pinc.sqsUrls[campus + 'Url'],
+        snsArn: pinc.snsArns[campus + 'Arn']
+    };
+}
 
 export const checkin: ECCHandlerFunction = async (_reqkey, datax, _ecc) => {
     // Get parameters from incomming data buffer\
     const timestampHold = DateTime.now();
     const rpgFields = pncchkinapi.convertCheckinDSToObject(datax);
+    const { snsArn } = getCampusAws(rpgFields.campus);
     const reqFields = {
         event: 'yardhound.import_events.checkin',
         time: timestampHold.toFormat("yyyy-MM-dd'T'TTZZ"),
@@ -56,7 +112,8 @@ export const checkin: ECCHandlerFunction = async (_reqkey, datax, _ecc) => {
             Message: JSON.stringify(reqFields),
             MessageGroupId: rpgFields.message_group_id,
             MessageDeduplicationId: uuidv4(),
-            TopicArn: pinc.sns.prdTargetArn
+            // TopicArn: pinc.sns.prdTargetArn
+            TopicArn: snsArn
         };
 
         // Create promise and SNS service object
@@ -70,7 +127,8 @@ export const checkin: ECCHandlerFunction = async (_reqkey, datax, _ecc) => {
     }
 
     const record = {
-        targetArn: pinc.sns.prdTargetArn,
+        // targetArn: pinc.sns.prdTargetArn,
+        targetArn: snsArn,
         messageGrpId: rpgFields.message_group_id,
         message: JSON.stringify(reqFields),
         result: response.message, // JSON.stringify(result) ?? "No Response",
@@ -87,52 +145,60 @@ export const latlon: ECCHandlerFunction = async (reqkey, _data, ecc) => {
     const timestampHold = DateTime.now();
     let nextReqKey = reqkey;
     let result: pnclatlonapi.LLRes;
+    const reqFields = pnclatlonapi.convertLLReqToObject(_data);
     try {
-        const response = await sqs.receiveMessage(_.omit(['apiVersion'], pinc.sqs) as any).promise();
+        const { sqsUrl } = getCampusAws(reqFields.facility);
+        const sqsConfig = {
+            ...pinc.sqs,
+            QueueUrl: sqsUrl
+        };
+        // const response = await sqs.receiveMessage(_.omit(['apiVersion'], pinc.sqs) as any).promise(); // Pinc.sqs = sqsConfig???
+        const response = await sqs.receiveMessage(_.omit(['apiVersion'], sqsConfig) as any).promise();
         logger.debug('Receive Message Result', response);
 
         let message = response?.Messages && response.Messages[0];
         // message = undefined here if nothing  received
         if (message !== undefined) {
-        result = JSON.parse(message?.Body || '{}');
-        if (message && message.ReceiptHandle && result?.data?.asset) {
-            result.data.asset.checked_out ||= '';
+            result = JSON.parse(message?.Body || '{}');
+            // @ts-ignore
+            const dataarray = JSON.parse(result.Message);            
+            if (message && message.ReceiptHandle && dataarray.data) {
+                dataarray.event ||= '';
 
-            const deleteParams = {
-                QueueUrl: pinc.sqs.QueueUrl,
-                ReceiptHandle: message.ReceiptHandle
+                const deleteParams = {
+                    // QueueUrl: pinc.sqs.QueueUrl,
+                    QueueUrl: sqsUrl,
+                    ReceiptHandle: message.ReceiptHandle
+                };
+                const deleteResult = await sqs.deleteMessage(deleteParams).promise();
+                logger.debug('Delete Result', deleteResult);
+            } else {
+                logger.warn('Received no valid messages', message);
+                return ecc.sendEccResult('ECC2000', 'No Valid Messages to Receive', nextReqKey);
+            }
+
+            logger.debug('SQS Message Receive Sent', result);
+
+            const record2 = {
+                queueUrl: sqsUrl,
+                resultCode: response.$response.httpResponse.statusCode,
+                result: JSON.stringify(dataarray) ?? "No Response", //result, 
+                timestamp: timestampHold.toFormat("yyyy-MM-dd'-'HH.mm.ss.SSS'000'")
             };
-            const deleteResult = await sqs.deleteMessage(deleteParams).promise();
-            logger.debug('Delete Result', deleteResult);
+            transport
+                .execute(insertPincSqsLog, record2)
+                .catch((err) => logger.error('Failed to write Pinc Log Sqs LatLon Record', { record2, err }));
+                let responseData = sanitizeValues(dataarray, safeValues);
+            nextReqKey = await ecc.sendEccResult('ECC0000', 'Success', nextReqKey);
+            return await ecc.sendObjectToCaller(responseData, pnclatlonapi.convertObjectToLLRes, nextReqKey);
         } else {
             logger.warn('Received no valid messages', message);
             return ecc.sendEccResult('ECC2000', 'No Valid Messages to Receive', nextReqKey);
         }
-
-        logger.debug('SQS Message Receive Sent', result);
-
-
-        const record2 = {
-            targetArn: pinc.sqs.QueueUrl,
-            result: result, // JSON.stringify(result) ?? "No Response",
-            resultCode: nextReqKey,
-            timestamp: timestampHold.toFormat("yyyy-MM-dd'-'HH.mm.ss.SSS'000'")
-        }
-        transport
-            .execute(insertPincSqsLog, record2)
-            .catch((err) => logger.error('Failed to write Pinc Log Sqs Checkout Record', { record2, err }));
-            nextReqKey = await ecc.sendEccResult('ECC0000', 'Success', nextReqKey);
-            return await ecc.sendObjectToCaller(result, pnclatlonapi.convertObjectToLLRes, nextReqKey);                    
-    } else {
-        logger.warn('Received no valid messages', message);
-        return ecc.sendEccResult('ECC2000', 'No Valid Messages to Receive', nextReqKey);
-    }
-
     } catch (err) {
         logger.warn('SQS Message Receive Failed', err);
         return ecc.sendEccResult('ECC9000', err.message, nextReqKey);
     }
-
 };
 export const errors: ECCHandlerFunction = async (reqkey, _data, ecc) => {
     // Call web service
@@ -140,40 +206,46 @@ export const errors: ECCHandlerFunction = async (reqkey, _data, ecc) => {
     let result: pncerrorapi.LLErrRes;
     const reqFields = pncerrorapi.convertLLErrReqToObject(_data);
     try {
-        const response = await sqs.receiveMessage(_.omit(['apiVersion'], pinc.sqserr) as any).promise();
+        const sqsConfig = {
+            ...pinc.sqs,
+            QueueUrl: pinc.sqsUrls.errurl
+        };
+        // const response = await sqs.receiveMessage(_.omit(['apiVersion'], pinc.sqserr) as any).promise();
+        const response = await sqs.receiveMessage(_.omit(['apiVersion'], sqsConfig) as any).promise();
         logger.debug('Receive Message Result', response);
-        
+
         let message = response?.Messages && response.Messages[0];
         // message = undefined here if nothing  received
         if (message !== undefined) {
-        result = JSON.parse(message?.Body || '{}');
-        //@ts-ignore
-        const dataarray = JSON.parse(result.Message);
-            //let resultdata = JSON.parse(result);
-        if (message && message.ReceiptHandle && dataarray.data) {
-            dataarray.event ||= '';
+            result = JSON.parse(message?.Body || '{}');
+            // @ts-ignore
+            const dataarray = JSON.parse(result.Message);
+            // let resultdata = JSON.parse(result);
+            if (message && message.ReceiptHandle && dataarray.data) {
+                dataarray.event ||= '';
 
-            const deleteParams = {
-                QueueUrl: pinc.sqserr.QueueUrl,
-                ReceiptHandle: message.ReceiptHandle
-            };
-            const deleteResult = await sqs.deleteMessage(deleteParams).promise();
-            logger.debug('Delete Result', deleteResult);
-        } else {
-            logger.warn('Received no valid messages', message);
-            return ecc.sendEccResult('ECC2000', 'No Valid Messages to Receive', nextReqKey);
-        }
-        try {
-            let dataout = JSON.stringify(dataarray);
-            await fs.writeFile(reqFields.filename, dataout, 'utf-8');
-        } catch (err) {
-            logger.warn('fs.write failed in sqs error receive',err.message);
-            return ecc.sendEccResult('ECC9200', err.message, nextReqKey);
-        }
-        logger.debug('SQS Message Receive Sent', result);
-        return  await ecc.sendEccResult('ECC0000', 'Success', nextReqKey);
-        // nextReqKey = await ecc.sendEccResult('ECC0000', 'Success', nextReqKey);
-        // return await ecc.sendObjectToCaller(result, pncerrorapi.convertObjectToLLErrRes, nextReqKey);
+                const deleteParams = {
+                    // QueueUrl: pinc.sqserr.QueueUrl,
+                    QueueUrl: pinc.sqsUrls.errurl,
+                    ReceiptHandle: message.ReceiptHandle
+                };
+                const deleteResult = await sqs.deleteMessage(deleteParams).promise();
+                logger.debug('Delete Result', deleteResult);
+            } else {
+                logger.warn('Received no valid messages', message);
+                return ecc.sendEccResult('ECC2000', 'No Valid Messages to Receive', nextReqKey);
+            }
+            try {
+                let dataout = JSON.stringify(dataarray);
+                await fs.writeFile(reqFields.filename, dataout, 'utf-8');
+            } catch (err) {
+                logger.warn('fs.write failed in sqs error receive', err.message);
+                return ecc.sendEccResult('ECC9200', err.message, nextReqKey);
+            }
+            logger.debug('SQS Message Receive Sent', result);
+            return await ecc.sendEccResult('ECC0000', 'Success', nextReqKey);
+            // nextReqKey = await ecc.sendEccResult('ECC0000', 'Success', nextReqKey);
+            // return await ecc.sendObjectToCaller(result, pncerrorapi.convertObjectToLLErrRes, nextReqKey);
         } else {
             logger.warn('Received no valid messages', message);
             return ecc.sendEccResult('ECC2000', 'No Valid Messages to Receive', nextReqKey);
@@ -188,6 +260,8 @@ export const checkot: ECCHandlerFunction = async (_reqkey, datax, _ecc) => {
     // Get parameters from incomming data buffer
     const timestampHold = DateTime.now();
     const rpgFields = pncchkotapi.convertCheckotDSToObject(datax);
+    const { snsArn } = getCampusAws(rpgFields.campus);
+
     const reqFields = {
         event: 'yardhound.import_events.checkout',
         time: timestampHold.toFormat("yyyy-MM-dd'T'TTZZ"),
@@ -219,7 +293,8 @@ export const checkot: ECCHandlerFunction = async (_reqkey, datax, _ecc) => {
             Message: JSON.stringify(reqFields),
             MessageGroupId: rpgFields.message_group_id,
             MessageDeduplicationId: uuidv4(),
-            TopicArn: pinc.sns.prdTargetArn
+            // TopicArn: pinc.sns.prdTargetArn
+            TopicArn: snsArn
         };
 
         // Create promise and SNS service object
@@ -233,7 +308,8 @@ export const checkot: ECCHandlerFunction = async (_reqkey, datax, _ecc) => {
     }
 
     const record = {
-        targetArn: pinc.sns.prdTargetArn,
+        // targetArn: pinc.sns.prdTargetArn,
+        targetArn: snsArn,
         messageGrpId: rpgFields.message_group_id,
         message: JSON.stringify(reqFields),
         result: response.message, // JSON.stringify(result) ?? "No Response",
@@ -249,6 +325,7 @@ export const updat: ECCHandlerFunction = async (_reqkey, datax, _ecc) => {
     // Get parameters from incomming data buffer
     const timestampHold = DateTime.now();
     const rpgFields = pncupdatapi.convertUpdatDSToObject(datax);
+    const { snsArn } = getCampusAws(rpgFields.campus);
     const reqFields = {
         event: 'yardhound.import_events.update',
         time: timestampHold.toFormat("yyyy-MM-dd'T'TTZZ"),
@@ -280,7 +357,8 @@ export const updat: ECCHandlerFunction = async (_reqkey, datax, _ecc) => {
             Message: JSON.stringify(reqFields),
             MessageGroupId: rpgFields.message_group_id,
             MessageDeduplicationId: uuidv4(),
-            TopicArn: pinc.sns.prdTargetArn
+            // TopicArn: pinc.sns.prdTargetArn
+            TopicArn: snsArn
         };
 
         // Create promise and SNS service object
@@ -294,7 +372,8 @@ export const updat: ECCHandlerFunction = async (_reqkey, datax, _ecc) => {
     }
 
     const record = {
-        targetArn: pinc.sns.prdTargetArn,
+        // targetArn: pinc.sns.prdTargetArn,
+        targetArn: snsArn,
         messageGrpId: rpgFields.message_group_id,
         message: JSON.stringify(reqFields),
         result: response.message, // JSON.stringify(result) ?? "No Response",
@@ -312,6 +391,7 @@ export const locat: ECCHandlerFunction = async (_reqkey, datax, _ecc) => {
     // Get parameters from incomming data buffer
     const timestampHold = DateTime.now();
     const rpgFields = pnclocatapi.convertLocatDSToObject(datax);
+    const { snsArn } = getCampusAws(rpgFields.campus);
     const reqFields = {
         event: 'yardhound.import_events.retrieve_asset_location',
         time: timestampHold.toFormat("yyyy-MM-dd'T'TTZZ"),
@@ -344,7 +424,8 @@ export const locat: ECCHandlerFunction = async (_reqkey, datax, _ecc) => {
             Message: JSON.stringify(reqFields),
             MessageGroupId: rpgFields.message_group_id,
             MessageDeduplicationId: uuidv4(),
-            TopicArn: pinc.sns.prdTargetArn
+            // TopicArn: pinc.sns.prdTargetArn
+            TopicArn: snsArn
         };
 
         // Create promise and SNS service object
@@ -358,7 +439,8 @@ export const locat: ECCHandlerFunction = async (_reqkey, datax, _ecc) => {
     }
 
     const record = {
-        targetArn: pinc.sns.prdTargetArn,
+        // targetArn: pinc.sns.prdTargetArn,
+        targetArn: snsArn,
         messageGrpId: rpgFields.message_group_id,
         message: JSON.stringify(reqFields),
         result: response.message, // JSON.stringify(result) ?? "No Response",
