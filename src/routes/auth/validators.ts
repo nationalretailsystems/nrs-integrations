3fd93c0c086871aded95a71e5298c46927301717
mcorbo <mikecorbo@verizon.net>
2022-04-12 15:25:31 -0400

    fourkites xauth

--- src/routes/auth/validators.ts
+++ src/routes/auth/validators.ts
@@ -1,13 +1,11 @@
-import { body, oneOf } from 'express-validator';
+import { body } from 'express-validator';
 import config from 'config';
 import { InputCheckChain } from 'src/types';
 const regexes = config.get().regexes;
 
 export const login: InputCheckChain[] = [
-    oneOf([body('username').exists().isString(),
-    body('password').exists().isString().matches(regexes.password)
-],[
-    body('client_id').exists().isString(),
-    body('client_secret').exists().isString().matches(regexes.password)
+        body('username').exists().isString(),
+        body('password').exists().isString().matches(regexes.password)
 ]
-)];
+
+
