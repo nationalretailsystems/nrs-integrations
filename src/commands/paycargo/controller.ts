a5d47e474a47a660a48fb9e37fe69fa76a0e5782
mcorbo <mikecorbo@verizon.net>
2022-11-03 07:30:49 -0400

    Paycargo getTransactions

--- src/commands/paycargo/controller.ts
+++ src/commands/paycargo/controller.ts
@@ -2,74 +2,14 @@ import { ECCHandlerFunction } from '@eradani-inc/ecc-router/types';
 import axios from 'axios';
 import config from 'config';
 import createLogger from 'src/services/logger';
-import * as converterusraut from 'src/interfaces/pcusraut';
 import * as converterachrpt from 'src/interfaces/pcachrpt';
 import * as convertergettran from 'src/interfaces/pcgettran';
+import * as convertergettrns from 'src/interfaces/pcgettrns';
 import { getToken } from 'src/services/get-token';
 
 const logger = createLogger('commands/paycargo');
 const { paycargo_dev } = config;
 const axiosInstance = axios.create(paycargo_dev.axios);
-
-export const getUsraut: ECCHandlerFunction = async function (reqkey, datax, ecc) {
-    // Get parameters from incomming data buffer
-    // const reqFields = converterusraut.convertPCReqTokenToObject(datax);
-    const reqFields = {
-        username: paycargo_dev.username,
-        password: paycargo_dev .password
-    }
-    logger.debug(`Received getPayCargoUser request`, { reqkey, datax });
-
-    // Call web service
-    let result;
-    let nextReqKey = reqkey;
-    // const jsonData = reqFields;
-      try {
-        const token = await getToken()
-        result = await axiosInstance.post('/login', reqFields, {
-            headers: {
-                Authorization: `JWT ${token}`
-            }
-        })
-       // result = await axiosInstance.post('/login', reqFields,   {
-       //     headers: {
-       //         'Content-Type': 'application/x-www-form-urlencoded'
-       //     }
-       // });
-        // });        
-        // result = await axiosInstance.get('/v4/address/validate', {
-        //     params: {
-        //          ...reqFields
-        //     },
-        //     headers: {
-        //         Accept: 'application/json',
-        //         Authorization: mailgun.apikey
-        //     }
-        // });
-    } catch (err) {
-        if (err.response) {
-            // If the request was made and the server responded with a status code
-            // That falls out of the range of 2xx
-            // Note: These error formats are dependent on the web service
-            return ecc.sendEccResult('ECC1000', err.response.status + '-' + err.response.statusText, nextReqKey);
-        }
-
-        // Else the request was made but no response was received
-        // Note: This error format has nothing to do with the web service. This is
-        // Mainly TCP/IP errors.
-        return ecc.sendEccResult('ECC1000', err.message, nextReqKey);
-    }
-
-    // Else save the joke then change the value field so it is as expected
-    // Note: if successful value is an object containing the joke and other info
-
-    // const responseData= result;
-    // Send the result info
-    nextReqKey = await ecc.sendEccResult('ECC0000', 'Success', nextReqKey);
-
-    // Send the results
-    return ecc.sendObjectToCaller(result.data, converterusraut.convertObjectToPCRcvToken, nextReqKey);
-};
 
 export const getAchrpt: ECCHandlerFunction = async function (reqkey, datax, ecc) {
     // Get parameters from incomming data buffer
@@ -137,7 +77,7 @@ export const getTransaction: ECCHandlerFunction = async function (reqkey, datax,
     // const jsonData = reqFields;
       try {
         const token = await getToken()
-        result = await axiosInstance.post('/transaction/' + reqFields.transactionId, {
+        result = await axiosInstance.get('/transaction/' + reqFields.transactionId, {
             headers: {
                 Authorization: `JWT ${token}`
             }
@@ -162,5 +102,49 @@ export const getTransaction: ECCHandlerFunction = async function (reqkey, datax,
     nextReqKey = await ecc.sendEccResult('ECC0000', 'Success', nextReqKey);
 
     // Send the results
-    return ecc.sendObjectToCaller(result, convertergettran.convertObjectToPCRcvTrn, nextReqKey);
+    return ecc.sendObjectToCaller(result.data, convertergettran.convertObjectToPCRcvTrn, nextReqKey);
+};
+export const getTransactions: ECCHandlerFunction = async function (reqkey, datax, ecc) {
+    // Get parameters from incomming data buffer
+    // const reqFields = converterusraut.convertPCReqTokenToObject(datax);
+    const reqFields = convertergettrns.convertPCReqTrnsToObject(datax);
+
+    logger.debug(`Received getTransactions request`, { reqkey, datax });
+
+    // Call web service
+    let result;
+    let nextReqKey = reqkey;
+    // const jsonData = reqFields;
+      try {
+        const token = await getToken()
+        result = await axiosInstance.get('/transactions' ,  {
+            params: {
+                page: reqFields.page,
+                count: reqFields.count
+            },
+            headers: {
+                Authorization: `JWT ${token}`
+            }
+        })
+
+    } catch (err) {
+        if (err.response) {
+            // If the request was made and the server responded with a status code
+            // That falls out of the range of 2xx
+            // Note: These error formats are dependent on the web service
+            return ecc.sendEccResult('ECC1000', err.response.status + '-' + err.response.statusText, nextReqKey);
+        }
+
+        // Else the request was made but no response was received
+        // Note: This error format has nothing to do with the web service. This is
+        // Mainly TCP/IP errors.
+        return ecc.sendEccResult('ECC1000', err.message, nextReqKey);
+    }
+
+    // const responseData= result;
+    // Send the result info
+    nextReqKey = await ecc.sendEccResult('ECC0000', 'Success', nextReqKey);
+
+    // Send the results
+    return ecc.sendObjectToCaller(result.data, convertergettrns.convertObjectToPCRcvTrns, nextReqKey);
 };
--- src/commands/paycargo/index.ts
+++ src/commands/paycargo/index.ts
@@ -1,11 +1,11 @@
 import { ECCInternalRouter } from '@eradani-inc/ecc-router/ecc-router';
-import { getUsraut, getAchrpt, getTransaction } from './controller';
-import * as pcusraut from 'src/interfaces/pcusraut';
+import { getAchrpt, getTransaction, getTransactions } from './controller';
 import * as pcachrpt from 'src/interfaces/pcachrpt';
 import * as pcgettran from 'src/interfaces/pcgettran';
+import * as pcgettrns from 'src/interfaces/pcgettrns';
 
 export default function registerPayCargo(router: ECCInternalRouter) {
-    router.use('paycargo', pcusraut, getUsraut);
     router.use('achrpt', pcachrpt, getAchrpt);
     router.use('gettran',pcgettran, getTransaction)
+    router.use('gettrns',pcgettrns, getTransactions)    
 }
