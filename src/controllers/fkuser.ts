6ac715f75061a01d2793bcda49acb65388d3b146
bStrubeEradani <ben@eradani.com>
2023-03-22 21:43:11 +0000

    refactored from PR comments

--- src/controllers/fkuser.ts
+++ src/controllers/fkuser.ts
@@ -20,8 +20,7 @@ export function generateJWT(userData: JWTUserData) {
     };
 
     return jwt.sign(user).then((token: string) => {
-        /* eslint-disable */
+        /* eslint-disable-next-line camelcase */
         return { access_token: token };
-        /* eslint-enable */
     });
 }
