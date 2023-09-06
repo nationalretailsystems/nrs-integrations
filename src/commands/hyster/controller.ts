5e8d42d6a6f915c3b7d7f91ac2a0e2a20a0c1498
Mike Corbo <mike.corbo@nrsonline.com>
2023-09-06 13:05:59 +0000

    Hyster Changes

--- src/commands/hyster/controller.ts
+++ src/commands/hyster/controller.ts
@@ -20,17 +20,18 @@ export const getLatestSessions: ECCHandlerFunction = async (reqkey, data, ecc) =
     // let reqDate = reqFields.sincedate.toISOString().split('T')[0];
     // Call web service
     let result;
+    let record;
     let nextReqKey = reqkey;
-    let startDate = '10-AUG-2023 00:00:00';
-    let endDate = '10-AUG-2023 23:59:59';
     let dataParm =
         '<Envelope xmlns="http://schemas.xmlsoap.org/soap/envelope/">' +
         '<Body>' +
         '<GetLatestSessions xmlns="http://www.nmhg.com">' +
         '<username>' +
         config.hyster.username +
+        '</username>' +
         '<password>' +
         config.hyster.password +
+        '</password>' +
         '<startDateEq>' +
         reqFields.reqDate + ' 00:00:00' +
         '</startDateEq>' +
@@ -41,14 +42,15 @@ export const getLatestSessions: ECCHandlerFunction = async (reqkey, data, ecc) =
         '</GetLatestSessions>' +
         '</Body>' +
         '</Envelope>';
+    const headers = {
+        'Content-Type': 'text/xml',
+        'SOAPAction': '"http://www.nmhg.com/INTPWebService/GetLatestSessions"'
+    }
+
     try {
-        result = await axiosInstance.post('', {
-            headers: {
-                'Content-Type': 'text/xml',
-                SOAPAction: '"http://www.nmhg.com/INTPWebService/GetLatestSessions"'
-            },
-            data: dataParm
-        });
+        result = await axiosInstance.post('/NTPWebService.svc', dataParm, {
+            headers: headers
+            });
     } catch (err) {
         if (err.response) {
             // If the request was made and the server responded with a status code
@@ -64,6 +66,25 @@ export const getLatestSessions: ECCHandlerFunction = async (reqkey, data, ecc) =
     }
     try {
         let jObj = parser.parse(result.data);
+        let sessionEvents =
+        jObj['s:Envelope']['s:Body']['GetBulkSessionsByDatesResponse']['GetBulkSessionsByDatesResult']['a:SessionEvent'];
+    // Break records into chunks using lodash
+    let sessionChunks = _.chunk(sessionEvents, 400);
+    // Write records to database using ec-bulk
+    let promises = [];
+    for (let chunk of sessionChunks) {
+        // record = {
+            // targetArn: pinc.sns.prdTargetArn,
+            // SessionID: chunk.sessionid,
+            // messageGrpId: ,
+            // message: JSON.stringify(reqFields),
+            // result: response.message, // JSON.stringify(result) ?? "No Response",
+            // resultCode: response.resultCode,
+            // timestamp: timestampHold.toFormat("yyyy-MM-dd'-'HH.mm.ss.SSS'000'") 
+       //  };
+       // promises.push(bulk.execute(transport, sqlModel, record));
+     }
+    // await Promise.all(promises);        
         logger.debug('ECC0000', 'Success', nextReqKey);
         nextReqKey = await ecc.sendEccResult('ECC0000', 'Success', nextReqKey);
         // nextReqKey = await ecc.sendObjectsToCaller(responseData, converter.convertObjectToAssetChgDS, nextReqKey);
---
+++ src/models/hyster.ts
@@ -0,0 +1,20 @@
+import config from 'config';
+import ec from '@eradani-inc/eradani-connect';
+import { JSONObject } from 'src/types';
+
+export const insertHysterRecord = new ec.run.Sql(
+    `insert into ${config.native.hysterLibrary}.HysterLog values (?, ?, ?, ?, ?, ?,? ?)`,
+    {
+        params: [
+            { name: 'SessionID', type: new ec.dataTypes.Char(15) },
+            { name: 'EquipmentAssetID', type: new ec.dataTypes.Char(15) },
+            { name: 'StartDateTimeEq', type: new ec.dataTypes.Timestamp() },
+            { name: 'EndDateTimeEq', type: new ec.dataTypes.Timestamp() },
+            { name: 'StartMainServiceMeter', type: new ec.dataTypes.Char(15) },
+            { name: 'EndMainServiceMeter', type: new ec.dataTypes.Char(15) },
+            { name: 'UpdateFlag', type: new ec.dataTypes.Char(1) },
+            { name: 'UpdateStamp', type: new ec.dataTypes.Timestamp() }
+        ]
+    }
+);
+
