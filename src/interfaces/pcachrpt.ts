e6bd05df508457392ea92d95a30d123d24d2ec10
Mike Corbo <mike.corbo@nrsonline.com>
2023-09-11 13:07:36 +0000

    Paycargo Changes

--- src/interfaces/pcachrpt.ts
+++ src/interfaces/pcachrpt.ts
@@ -3,10 +3,17 @@
 // Module: pcachrpt
 // Generated source -- do not modify
 
-import { ibmiConversions, missingInput } from '@eradani-inc/ec-client';
-const { fromIbmiDate, fromIbmiTime, fromIbmiTimestamp, toIbmiDate, toIbmiTime, toIbmiTimestamp } = ibmiConversions;
+import { ibmiConversions, missingInput } from "@eradani-inc/ec-client";
+const {
+  fromIbmiDate,
+  fromIbmiTime,
+  fromIbmiTimestamp,
+  toIbmiDate,
+  toIbmiTime,
+  toIbmiTimestamp
+} = ibmiConversions;
 
-import eradaniConnect from '@eradani-inc/eradani-connect';
+import eradaniConnect from "@eradani-inc/eradani-connect";
 const { dataTypes } = eradaniConnect;
 
 /**
@@ -16,31 +23,31 @@ export interface ParmsDS {
     /**
      * @size 6 characters
      */
-    branchClientId: string;
+    branchClientId: string,
     /**
      * @size 10 characters
      */
-    startDate: string;
+    startDate: string,
     /**
      * @size 10 characters
      */
-    endDate: string;
+    endDate: string,
     /**
      * @size 6 characters
      */
-    client_id: string;
+    client_id: string,
     /**
      * @size 50 characters
      */
-    report_action_name: string;
+    report_action_name: string,
     /**
      * @size 10 characters
      */
-    type: string;
+    type: string,
     /**
      * @size 10 characters
      */
-    accountName: string;
+    accountName: string
 }
 
 /**
@@ -49,171 +56,171 @@ export interface ParmsDS {
 export interface RecSetDS {
     /**
      * @size 10 characters
-     * @default ` `
+     * @default ``
      */
-    account: string;
+    account: string,
     /**
      * @size 10 digits
      * @default `0`
      */
-    transaction_id: number;
+    transaction_id: number,
     /**
      * @size 50 characters
-     * @default ` `
+     * @default ``
      */
-    payor: string;
+    payor: string,
     /**
      * @size 50 characters
-     * @default ` `
+     * @default ``
      */
-    payee: string;
+    payee: string,
     /**
      * @size 25 characters
-     * @default ` `
+     * @default ``
      */
-    category: string;
+    category: string,
     /**
      * @size 25 characters
-     * @default ` `
+     * @default ``
      */
-    transaction_type: string;
+    transaction_type: string,
     /**
      * @size 25 characters
-     * @default ` `
+     * @default ``
      */
-    number: string;
+    number: string,
     /**
      * @size 10 characters
-     * @default ` `
+     * @default ``
      */
-    debitCredit: string;
+    debitCredit: string,
     /**
      * @size 15 digits
      * @precision 5 decimals
      * @default `0`
      */
-    debitAmount: number;
+    debitAmount: number,
     /**
      * @size 15 digits
      * @precision 5 decimals
      * @default `0`
      */
-    creditAmount: number;
+    creditAmount: number,
     /**
      * @size 10 digits
      * @default `0`
      */
-    payorBatch: number;
+    payorBatch: number,
     /**
      * @size 24 characters
-     * @default ` `
+     * @default ``
      */
-    payorDate: string;
+    payorDate: string,
     /**
      * @size 10 digits
      * @default `0`
      */
-    payeeBatch: number;
+    payeeBatch: number,
     /**
      * @size 24 characters
-     * @default ` `
+     * @default ``
      */
-    payeeDate: string;
+    payeeDate: string,
     /**
      * @size 10 characters
-     * @default ` `
+     * @default ``
      */
-    payorID: string;
+    payorID: string,
     /**
      * @size 10 characters
-     * @default ` `
+     * @default ``
      */
-    payeeID: string;
+    payeeID: string,
     /**
      * @size 25 characters
-     * @default ` `
+     * @default ``
      */
-    customer_reference_number: string;
+    customer_reference_number: string,
     /**
      * @size 25 characters
-     * @default ` `
+     * @default ``
      */
-    shipper_reference_number: string;
+    shipper_reference_number: string,
     /**
      * @size 25 characters
-     * @default ` `
+     * @default ``
      */
-    parent: string;
+    parent: string,
     /**
      * @size 24 characters
-     * @default ` `
+     * @default ``
      */
-    approvalDate: string;
+    approvalDate: string,
     /**
      * @size 25 characters
-     * @default ` `
+     * @default ``
      */
-    approvedByUserName: string;
+    approvedByUserName: string,
     /**
      * @size 3 characters
-     * @default ` `
+     * @default ``
      */
-    currency: string;
+    currency: string,
     /**
      * @size 10 digits
      * @default `0`
      */
-    payerId: number;
+    payerId: number,
     /**
      * @size 10 digits
      * @default `0`
      */
-    vendorId: number;
+    vendorId: number,
     /**
      * @size 10 characters
-     * @default ` `
+     * @default ``
      */
-    payerReferenceNumber: string;
+    payerReferenceNumber: string,
     /**
      * @size 25 characters
-     * @default ` `
+     * @default ``
      */
-    origin: string;
+    origin: string,
     /**
      * @size 25 characters
-     * @default ` `
+     * @default ``
      */
-    destination: string;
+    destination: string,
     /**
      * @size 25 characters
-     * @default ` `
+     * @default ``
      */
-    payerInternalNumber: string;
+    payerInternalNumber: string,
     /**
      * @size 25 characters
-     * @default ` `
+     * @default ``
      */
-    payerFileNumber: string;
+    payerFileNumber: string,
     /**
      * @size 25 characters
-     * @default ` `
+     * @default ``
      */
-    payerVoucherNumber: string;
+    payerVoucherNumber: string,
     /**
      * @size 25 characters
-     * @default ` `
+     * @default ``
      */
-    payerProductNumber: string;
+    payerProductNumber: string,
     /**
      * @size 25 characters
-     * @default ` `
+     * @default ``
      */
-    payerInvoiceNumber: string;
+    payerInvoiceNumber: string,
     /**
      * @size 50 characters
-     * @default ` `
+     * @default ``
      */
-    bolLink: string;
+    bolLink: string
 }
 
 /**
@@ -222,18 +229,21 @@ export interface RecSetDS {
 export interface OverallDS {
     /**
      * @size 10 digits
+     * @default `0`
      */
-    count: number;
+    count: number,
     /**
      * @size 15 digits
      * @precision 5 decimals
+     * @default `0`
      */
-    debitamount: number;
+    debitamount: number,
     /**
      * @size 15 digits
      * @precision 5 decimals
+     * @default `0`
      */
-    creditamount: number;
+    creditamount: number
 }
 
 /**
@@ -242,13 +252,15 @@ export interface OverallDS {
 export interface PayorDS {
     /**
      * @size 10 digits
+     * @default `0`
      */
-    batch: number;
+    batch: number,
     /**
      * @size 15 digits
      * @precision 5 decimals
+     * @default `0`
      */
-    amount: number;
+    amount: number
 }
 
 /**
@@ -257,13 +269,13 @@ export interface PayorDS {
 export interface DataDS {
     /**
      */
-    recordset: Array<RecSetDS>;
+    recordset: Array<RecSetDS>,
     /**
      */
-    overall: OverallDS;
+    overall: OverallDS,
     /**
      */
-    payor: PayorDS;
+    payor: PayorDS
 }
 
 /**
@@ -273,56 +285,58 @@ export interface PCReqRpt {
     /**
      * @size 10 characters
      */
-    branchClientId: string;
+    branchClientId: string,
     /**
      * @size 10 characters
      */
-    startDate: string;
+    startDate: string,
     /**
      * @size 10 characters
      */
-    endDate: string;
+    endDate: string,
     /**
      * @size 10 characters
      */
-    client_id: string;
+    client_id: string,
     /**
      * @size 25 characters
      */
-    report_action_name: string;
+    report_action_name: string,
     /**
      * @size 20 characters
      */
-    type: string;
+    type: string,
     /**
      * @size 10 characters
      */
-    accountName: string;
+    accountName: string
 }
 
 /**
  * Convert PCReqRpt record to TypeScript object
  */
 export function convertPCReqRptToObject(dataIn: string): PCReqRpt {
-    const dataOut: any = {};
-    let pos: number = 0;
+  const dataOut: any =   {
+  
+    };
+  let pos: number = 0;
 
-    dataOut.branchClientId = dataIn.substring(pos, pos + 10).trimEnd();
-    pos += 10;
-    dataOut.startDate = dataIn.substring(pos, pos + 10).trimEnd();
-    pos += 10;
-    dataOut.endDate = dataIn.substring(pos, pos + 10).trimEnd();
-    pos += 10;
-    dataOut.client_id = dataIn.substring(pos, pos + 10).trimEnd();
-    pos += 10;
-    dataOut.report_action_name = dataIn.substring(pos, pos + 25).trimEnd();
-    pos += 25;
-    dataOut.type = dataIn.substring(pos, pos + 20).trimEnd();
-    pos += 20;
-    dataOut.accountName = dataIn.substring(pos, pos + 10).trimEnd();
-    pos += 10;
+  dataOut.branchClientId = dataIn.substring(pos, pos + 10).trimEnd();
+  pos += 10;
+  dataOut.startDate = dataIn.substring(pos, pos + 10).trimEnd();
+  pos += 10;
+  dataOut.endDate = dataIn.substring(pos, pos + 10).trimEnd();
+  pos += 10;
+  dataOut.client_id = dataIn.substring(pos, pos + 10).trimEnd();
+  pos += 10;
+  dataOut.report_action_name = dataIn.substring(pos, pos + 25).trimEnd();
+  pos += 25;
+  dataOut.type = dataIn.substring(pos, pos + 20).trimEnd();
+  pos += 20;
+  dataOut.accountName = dataIn.substring(pos, pos + 10).trimEnd();
+  pos += 10;
 
-    return dataOut;
+  return dataOut;
 }
 
 /**
@@ -331,97 +345,63 @@ export function convertPCReqRptToObject(dataIn: string): PCReqRpt {
 export interface PCRcvRpt {
     /**
      */
-    recordset: Array<RecSetDS>;
+    recordset: Array<RecSetDS>,
     /**
      */
-    overall: OverallDS;
+    overall: OverallDS,
     /**
      */
-    payor: PayorDS;
+    payor: PayorDS
 }
 
 /**
  * Convert JavaScript object to PCRcvRpt record
  */
 export function convertObjectToPCRcvRpt(dataIn: PCRcvRpt): string {
-    let dataOut: string = '';
+  let dataOut: string = "";
 
-    for (let i: number = 0; i < 40; ++i) {
-        dataOut += dataIn?.recordset[i]?.account?.substring(0, 10)?.padEnd(10) ?? ' '.substring(0, 10).padEnd(10);
-        dataOut +=
-            dataIn?.recordset[i]?.transaction_id?.toFixed()?.substring(0, 11)?.padEnd(11) ??
-            '0'.substring(0, 11).padEnd(11);
-        dataOut += dataIn?.recordset[i]?.payor?.substring(0, 50)?.padEnd(50) ?? ' '.substring(0, 50).padEnd(50);
-        dataOut += dataIn?.recordset[i]?.payee?.substring(0, 50)?.padEnd(50) ?? ' '.substring(0, 50).padEnd(50);
-        dataOut += dataIn?.recordset[i]?.category?.substring(0, 25)?.padEnd(25) ?? ' '.substring(0, 25).padEnd(25);
-        dataOut +=
-            dataIn?.recordset[i]?.transaction_type?.substring(0, 25)?.padEnd(25) ?? ' '.substring(0, 25).padEnd(25);
-        dataOut += dataIn?.recordset[i]?.number?.substring(0, 25)?.padEnd(25) ?? ' '.substring(0, 25).padEnd(25);
-        dataOut += dataIn?.recordset[i]?.debitCredit?.substring(0, 10)?.padEnd(10) ?? ' '.substring(0, 10).padEnd(10);
-        dataOut +=
-            dataIn?.recordset[i]?.debitAmount?.toFixed(5)?.substring(0, 17)?.padEnd(17) ??
-            '0'.substring(0, 17).padEnd(17);
-        dataOut +=
-            dataIn?.recordset[i]?.creditAmount?.toFixed(5)?.substring(0, 17)?.padEnd(17) ??
-            '0'.substring(0, 17).padEnd(17);
-        dataOut +=
-            dataIn?.recordset[i]?.payorBatch?.toFixed()?.substring(0, 11)?.padEnd(11) ??
-            '0'.substring(0, 11).padEnd(11);
-        dataOut += dataIn?.recordset[i]?.payorDate?.substring(0, 24)?.padEnd(24) ?? ' '.substring(0, 24).padEnd(24);
-        dataOut +=
-            dataIn?.recordset[i]?.payeeBatch?.toFixed()?.substring(0, 11)?.padEnd(11) ??
-            '0'.substring(0, 11).padEnd(11);
-        dataOut += dataIn?.recordset[i]?.payeeDate?.substring(0, 24)?.padEnd(24) ?? ' '.substring(0, 24).padEnd(24);
-        dataOut += dataIn?.recordset[i]?.payorID?.substring(0, 10)?.padEnd(10) ?? ' '.substring(0, 10).padEnd(10);
-        dataOut += dataIn?.recordset[i]?.payeeID?.substring(0, 10)?.padEnd(10) ?? ' '.substring(0, 10).padEnd(10);
-        dataOut +=
-            dataIn?.recordset[i]?.customer_reference_number?.substring(0, 25)?.padEnd(25) ??
-            ' '.substring(0, 25).padEnd(25);
-        dataOut +=
-            dataIn?.recordset[i]?.shipper_reference_number?.substring(0, 25)?.padEnd(25) ??
-            ' '.substring(0, 25).padEnd(25);
-        dataOut += dataIn?.recordset[i]?.parent?.substring(0, 25)?.padEnd(25) ?? ' '.substring(0, 25).padEnd(25);
-        dataOut += dataIn?.recordset[i]?.approvalDate?.substring(0, 24)?.padEnd(24) ?? ' '.substring(0, 24).padEnd(24);
-        dataOut +=
-            dataIn?.recordset[i]?.approvedByUserName?.substring(0, 25)?.padEnd(25) ?? ' '.substring(0, 25).padEnd(25);
-        dataOut += dataIn?.recordset[i]?.currency?.substring(0, 3)?.padEnd(3) ?? ' '.substring(0, 3).padEnd(3);
-        dataOut +=
-            dataIn?.recordset[i]?.payerId?.toFixed()?.substring(0, 11)?.padEnd(11) ?? '0'.substring(0, 11).padEnd(11);
-        dataOut +=
-            dataIn?.recordset[i]?.vendorId?.toFixed()?.substring(0, 11)?.padEnd(11) ?? '0'.substring(0, 11).padEnd(11);
-        dataOut +=
-            dataIn?.recordset[i]?.payerReferenceNumber?.substring(0, 10)?.padEnd(10) ?? ' '.substring(0, 10).padEnd(10);
-        dataOut += dataIn?.recordset[i]?.origin?.substring(0, 25)?.padEnd(25) ?? ' '.substring(0, 25).padEnd(25);
-        dataOut += dataIn?.recordset[i]?.destination?.substring(0, 25)?.padEnd(25) ?? ' '.substring(0, 25).padEnd(25);
-        dataOut +=
-            dataIn?.recordset[i]?.payerInternalNumber?.substring(0, 25)?.padEnd(25) ?? ' '.substring(0, 25).padEnd(25);
-        dataOut +=
-            dataIn?.recordset[i]?.payerFileNumber?.substring(0, 25)?.padEnd(25) ?? ' '.substring(0, 25).padEnd(25);
-        dataOut +=
-            dataIn?.recordset[i]?.payerVoucherNumber?.substring(0, 25)?.padEnd(25) ?? ' '.substring(0, 25).padEnd(25);
-        dataOut +=
-            dataIn?.recordset[i]?.payerProductNumber?.substring(0, 25)?.padEnd(25) ?? ' '.substring(0, 25).padEnd(25);
-        dataOut +=
-            dataIn?.recordset[i]?.payerInvoiceNumber?.substring(0, 25)?.padEnd(25) ?? ' '.substring(0, 25).padEnd(25);
-        dataOut += dataIn?.recordset[i]?.bolLink?.substring(0, 50)?.padEnd(50) ?? ' '.substring(0, 50).padEnd(50);
-    }
-    dataOut +=
-        dataIn?.overall?.count?.toFixed()?.substring(0, 11)?.padEnd(11) ??
-        missingInput(`dataIn.overall.count`, 'integer', dataIn?.overall?.count);
-    dataOut +=
-        dataIn?.overall?.debitamount?.toFixed(5)?.substring(0, 17)?.padEnd(17) ??
-        missingInput(`dataIn.overall.debitamount`, 'packed', dataIn?.overall?.debitamount);
-    dataOut +=
-        dataIn?.overall?.creditamount?.toFixed(5)?.substring(0, 17)?.padEnd(17) ??
-        missingInput(`dataIn.overall.creditamount`, 'packed', dataIn?.overall?.creditamount);
-    dataOut +=
-        dataIn?.payor?.batch?.toFixed()?.substring(0, 11)?.padEnd(11) ??
-        missingInput(`dataIn.payor.batch`, 'integer', dataIn?.payor?.batch);
-    dataOut +=
-        dataIn?.payor?.amount?.toFixed(5)?.substring(0, 17)?.padEnd(17) ??
-        missingInput(`dataIn.payor.amount`, 'packed', dataIn?.payor?.amount);
+  for (let i: number = 0; i < 40; ++i) {
+  dataOut += dataIn?.recordset[i]?.account?.substring(0, 10)?.padEnd(10) ?? "".substring(0, 10).padEnd(10);
+  dataOut += dataIn?.recordset[i]?.transaction_id?.toFixed()?.substring(0, 11)?.padEnd(11) ?? "0".substring(0, 11).padEnd(11);
+  dataOut += dataIn?.recordset[i]?.payor?.substring(0, 50)?.padEnd(50) ?? "".substring(0, 50).padEnd(50);
+  dataOut += dataIn?.recordset[i]?.payee?.substring(0, 50)?.padEnd(50) ?? "".substring(0, 50).padEnd(50);
+  dataOut += dataIn?.recordset[i]?.category?.substring(0, 25)?.padEnd(25) ?? "".substring(0, 25).padEnd(25);
+  dataOut += dataIn?.recordset[i]?.transaction_type?.substring(0, 25)?.padEnd(25) ?? "".substring(0, 25).padEnd(25);
+  dataOut += dataIn?.recordset[i]?.number?.substring(0, 25)?.padEnd(25) ?? "".substring(0, 25).padEnd(25);
+  dataOut += dataIn?.recordset[i]?.debitCredit?.substring(0, 10)?.padEnd(10) ?? "".substring(0, 10).padEnd(10);
+  dataOut += dataIn?.recordset[i]?.debitAmount?.toFixed(5)?.substring(0, 17)?.padEnd(17) ?? "0".substring(0, 17).padEnd(17);
+  dataOut += dataIn?.recordset[i]?.creditAmount?.toFixed(5)?.substring(0, 17)?.padEnd(17) ?? "0".substring(0, 17).padEnd(17);
+  dataOut += dataIn?.recordset[i]?.payorBatch?.toFixed()?.substring(0, 11)?.padEnd(11) ?? "0".substring(0, 11).padEnd(11);
+  dataOut += dataIn?.recordset[i]?.payorDate?.substring(0, 24)?.padEnd(24) ?? "".substring(0, 24).padEnd(24);
+  dataOut += dataIn?.recordset[i]?.payeeBatch?.toFixed()?.substring(0, 11)?.padEnd(11) ?? "0".substring(0, 11).padEnd(11);
+  dataOut += dataIn?.recordset[i]?.payeeDate?.substring(0, 24)?.padEnd(24) ?? "".substring(0, 24).padEnd(24);
+  dataOut += dataIn?.recordset[i]?.payorID?.substring(0, 10)?.padEnd(10) ?? "".substring(0, 10).padEnd(10);
+  dataOut += dataIn?.recordset[i]?.payeeID?.substring(0, 10)?.padEnd(10) ?? "".substring(0, 10).padEnd(10);
+  dataOut += dataIn?.recordset[i]?.customer_reference_number?.substring(0, 25)?.padEnd(25) ?? "".substring(0, 25).padEnd(25);
+  dataOut += dataIn?.recordset[i]?.shipper_reference_number?.substring(0, 25)?.padEnd(25) ?? "".substring(0, 25).padEnd(25);
+  dataOut += dataIn?.recordset[i]?.parent?.substring(0, 25)?.padEnd(25) ?? "".substring(0, 25).padEnd(25);
+  dataOut += dataIn?.recordset[i]?.approvalDate?.substring(0, 24)?.padEnd(24) ?? "".substring(0, 24).padEnd(24);
+  dataOut += dataIn?.recordset[i]?.approvedByUserName?.substring(0, 25)?.padEnd(25) ?? "".substring(0, 25).padEnd(25);
+  dataOut += dataIn?.recordset[i]?.currency?.substring(0, 3)?.padEnd(3) ?? "".substring(0, 3).padEnd(3);
+  dataOut += dataIn?.recordset[i]?.payerId?.toFixed()?.substring(0, 11)?.padEnd(11) ?? "0".substring(0, 11).padEnd(11);
+  dataOut += dataIn?.recordset[i]?.vendorId?.toFixed()?.substring(0, 11)?.padEnd(11) ?? "0".substring(0, 11).padEnd(11);
+  dataOut += dataIn?.recordset[i]?.payerReferenceNumber?.substring(0, 10)?.padEnd(10) ?? "".substring(0, 10).padEnd(10);
+  dataOut += dataIn?.recordset[i]?.origin?.substring(0, 25)?.padEnd(25) ?? "".substring(0, 25).padEnd(25);
+  dataOut += dataIn?.recordset[i]?.destination?.substring(0, 25)?.padEnd(25) ?? "".substring(0, 25).padEnd(25);
+  dataOut += dataIn?.recordset[i]?.payerInternalNumber?.substring(0, 25)?.padEnd(25) ?? "".substring(0, 25).padEnd(25);
+  dataOut += dataIn?.recordset[i]?.payerFileNumber?.substring(0, 25)?.padEnd(25) ?? "".substring(0, 25).padEnd(25);
+  dataOut += dataIn?.recordset[i]?.payerVoucherNumber?.substring(0, 25)?.padEnd(25) ?? "".substring(0, 25).padEnd(25);
+  dataOut += dataIn?.recordset[i]?.payerProductNumber?.substring(0, 25)?.padEnd(25) ?? "".substring(0, 25).padEnd(25);
+  dataOut += dataIn?.recordset[i]?.payerInvoiceNumber?.substring(0, 25)?.padEnd(25) ?? "".substring(0, 25).padEnd(25);
+  dataOut += dataIn?.recordset[i]?.bolLink?.substring(0, 50)?.padEnd(50) ?? "".substring(0, 50).padEnd(50);
+  }
+  dataOut += dataIn?.overall?.count?.toFixed()?.substring(0, 11)?.padEnd(11) ?? "0".substring(0, 11).padEnd(11);
+  dataOut += dataIn?.overall?.debitamount?.toFixed(5)?.substring(0, 17)?.padEnd(17) ?? "0".substring(0, 17).padEnd(17);
+  dataOut += dataIn?.overall?.creditamount?.toFixed(5)?.substring(0, 17)?.padEnd(17) ?? "0".substring(0, 17).padEnd(17);
+  dataOut += dataIn?.payor?.batch?.toFixed()?.substring(0, 11)?.padEnd(11) ?? "0".substring(0, 11).padEnd(11);
+  dataOut += dataIn?.payor?.amount?.toFixed(5)?.substring(0, 17)?.padEnd(17) ?? "0".substring(0, 17).padEnd(17);
 
-    return dataOut;
+  return dataOut;
 }
 
 /* eslint-enable */
