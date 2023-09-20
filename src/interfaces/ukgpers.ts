9f9a6cad158705536dd3bb1c7039a2fedc31356f
Mike Corbo <mike.corbo@nrsonline.com>
2023-09-20 14:38:20 +0000

    person ukg

--- src/interfaces/ukgpers.ts
+++ src/interfaces/ukgpers.ts
@@ -46,6 +46,7 @@ export interface dataAccessExtensionGroupsDS {
      */
     dataAccessGroupRoleId: number,
     /**
+     * @default `false`
      */
     defaultSwitch: boolean,
     /**
@@ -157,6 +158,7 @@ export interface emailContactDataEntriesDS {
      */
     contactTypeId: number,
     /**
+     * @default `false`
      */
     hasEmailNotificationDelivery: boolean
 }
@@ -204,6 +206,7 @@ export interface identifierDS {
  */
 export interface licenseTypeListDS {
     /**
+     * @default `false`
      */
     active: boolean,
     /**
@@ -337,9 +340,11 @@ export interface preferenceProfileDataEntryDS {
      */
     schedulePeriodId: number,
     /**
+     * @default `false`
      */
     usingShiftLabel: boolean,
     /**
+     * @default `false`
      */
     usesTwelveHourFormat: boolean
 }
@@ -365,6 +370,7 @@ export interface telContactDataEntriesDS {
      */
     contactTypeId: number,
     /**
+     * @default `false`
      */
     smsswitch: boolean
 }
@@ -385,9 +391,11 @@ export interface employeeExtensionDS {
      */
     accessProfileId: number,
     /**
+     * @default `false`
      */
     accountLocked: boolean,
     /**
+     * @default `false`
      */
     active: boolean,
     /**
@@ -413,6 +421,7 @@ export interface employeeExtensionDS {
      */
     authenticationTypeId: number,
     /**
+     * @default `false`
      */
     clockOnlyUser: boolean,
     /**
@@ -468,9 +477,11 @@ export interface employeeExtensionDS {
      */
     employmentStatusesForExtensionSnapshotDate: string,
     /**
+     * @default `false`
      */
     faceRequiredFlag: boolean,
     /**
+     * @default `false`
      */
     fingerRequiredFlag: boolean,
     /**
@@ -484,6 +495,7 @@ export interface employeeExtensionDS {
      */
     fullName: string,
     /**
+     * @default `false`
      */
     hasKmailNotificationDelivery: boolean,
     /**
@@ -495,9 +507,11 @@ export interface employeeExtensionDS {
      */
     identifier: identifierDS,
     /**
+     * @default `false`
      */
     isEmployee: boolean,
     /**
+     * @default `false`
      */
     isManager: boolean,
     /**
@@ -542,6 +556,7 @@ export interface employeeExtensionDS {
      */
     logonProfileId: number,
     /**
+     * @default `false`
      */
     mfaRequired: boolean,
     /**
@@ -555,6 +570,7 @@ export interface employeeExtensionDS {
      */
     notificationProfile: string,
     /**
+     * @default `false`
      */
     passwordUpdateRequired: boolean,
     /**
@@ -601,9 +617,11 @@ export interface employeeExtensionDS {
      */
     processManagerProfile: string,
     /**
+     * @default `false`
      */
     requirePassChange: boolean,
     /**
+     * @default `false`
      */
     restrictedUser: boolean,
     /**
@@ -692,6 +710,7 @@ export interface personResp {
      */
     employeeExtension: employeeExtensionDS,
     /**
+     * @default `false`
      */
     exceptionPresent: boolean
 }
@@ -704,20 +723,20 @@ export function convertObjectTopersonResp(dataIn: personResp): string {
 
   dataOut += dataIn?.employeeExtension?.accessProfile?.substring(0, 12)?.padEnd(12) ?? "".substring(0, 12).padEnd(12);
   dataOut += dataIn?.employeeExtension?.accessProfileId?.toFixed(0)?.substring(0, 4)?.padEnd(4) ?? "0".substring(0, 4).padEnd(4);
-  dataOut += (dataIn?.employeeExtension?.accountLocked !== undefined ? (dataIn?.employeeExtension?.accountLocked ? "1" : "0") : missingInput(`dataIn.employeeExtension.accountLocked`, "bool", dataIn?.employeeExtension?.accountLocked));
-  dataOut += (dataIn?.employeeExtension?.active !== undefined ? (dataIn?.employeeExtension?.active ? "1" : "0") : missingInput(`dataIn.employeeExtension.active`, "bool", dataIn?.employeeExtension?.active));
+  dataOut += (dataIn?.employeeExtension?.accountLocked !== undefined ? (dataIn?.employeeExtension?.accountLocked ? "1" : "0") : "0");
+  dataOut += (dataIn?.employeeExtension?.active !== undefined ? (dataIn?.employeeExtension?.active ? "1" : "0") : "0");
   dataOut += dataIn?.employeeExtension?.approvalMethod?.substring(0, 15)?.padEnd(15) ?? "".substring(0, 15).padEnd(15);
   dataOut += dataIn?.employeeExtension?.approvalMethodId?.toFixed(0)?.substring(0, 3)?.padEnd(3) ?? "0".substring(0, 3).padEnd(3);
   dataOut += dataIn?.employeeExtension?.authenticationType?.substring(0, 9)?.padEnd(9) ?? "".substring(0, 9).padEnd(9);
   dataOut += dataIn?.employeeExtension?.authenticationTypeId?.toFixed(0)?.substring(0, 3)?.padEnd(3) ?? "0".substring(0, 3).padEnd(3);
-  dataOut += (dataIn?.employeeExtension?.clockOnlyUser !== undefined ? (dataIn?.employeeExtension?.clockOnlyUser ? "1" : "0") : missingInput(`dataIn.employeeExtension.clockOnlyUser`, "bool", dataIn?.employeeExtension?.clockOnlyUser));
+  dataOut += (dataIn?.employeeExtension?.clockOnlyUser !== undefined ? (dataIn?.employeeExtension?.clockOnlyUser ? "1" : "0") : "0");
   dataOut += dataIn?.employeeExtension?.consecutiveBadLogons?.toFixed(0)?.substring(0, 3)?.padEnd(3) ?? "0".substring(0, 3).padEnd(3);
   dataOut += dataIn?.employeeExtension?.dataAccessExtensionGroups?.assignDAGID?.toFixed(0)?.substring(0, 6)?.padEnd(6) ?? "0".substring(0, 6).padEnd(6);
   dataOut += dataIn?.employeeExtension?.dataAccessExtensionGroups?.dataAccessGroup?.substring(0, 10)?.padEnd(10) ?? "".substring(0, 10).padEnd(10);
   dataOut += dataIn?.employeeExtension?.dataAccessExtensionGroups?.dataAccessGroupId?.toFixed(0)?.substring(0, 4)?.padEnd(4) ?? "0".substring(0, 4).padEnd(4);
   dataOut += dataIn?.employeeExtension?.dataAccessExtensionGroups?.dataAccessGroupRole?.substring(0, 12)?.padEnd(12) ?? "".substring(0, 12).padEnd(12);
   dataOut += dataIn?.employeeExtension?.dataAccessExtensionGroups?.dataAccessGroupRoleId?.toFixed(0)?.substring(0, 4)?.padEnd(4) ?? "0".substring(0, 4).padEnd(4);
-  dataOut += (dataIn?.employeeExtension?.dataAccessExtensionGroups?.defaultSwitch !== undefined ? (dataIn?.employeeExtension?.dataAccessExtensionGroups?.defaultSwitch ? "1" : "0") : missingInput(`dataIn.employeeExtension.dataAccessExtensionGroups.defaultSwitch`, "bool", dataIn?.employeeExtension?.dataAccessExtensionGroups?.defaultSwitch));
+  dataOut += (dataIn?.employeeExtension?.dataAccessExtensionGroups?.defaultSwitch !== undefined ? (dataIn?.employeeExtension?.dataAccessExtensionGroups?.defaultSwitch ? "1" : "0") : "0");
   dataOut += dataIn?.employeeExtension?.dataAccessExtensionGroups?.effectiveDate?.substring(0, 10)?.padEnd(10) ?? "".substring(0, 10).padEnd(10);
   dataOut += dataIn?.employeeExtension?.dataAccessExtensionGroups?.expirationDate?.substring(0, 10)?.padEnd(10) ?? "".substring(0, 10).padEnd(10);
   dataOut += dataIn?.employeeExtension?.dataAccessExtensionGroups?.personID?.toFixed(0)?.substring(0, 8)?.padEnd(8) ?? "0".substring(0, 8).padEnd(8);
@@ -740,27 +759,27 @@ export function convertObjectTopersonResp(dataIn: personResp): string {
   dataOut += dataIn?.employeeExtension?.emailContactDataEntries?.contactData?.substring(0, 24)?.padEnd(24) ?? "".substring(0, 24).padEnd(24);
   dataOut += dataIn?.employeeExtension?.emailContactDataEntries?.contactType?.substring(0, 4)?.padEnd(4) ?? "".substring(0, 4).padEnd(4);
   dataOut += dataIn?.employeeExtension?.emailContactDataEntries?.contactTypeId?.toFixed(0)?.substring(0, 3)?.padEnd(3) ?? "0".substring(0, 3).padEnd(3);
-  dataOut += (dataIn?.employeeExtension?.emailContactDataEntries?.hasEmailNotificationDelivery !== undefined ? (dataIn?.employeeExtension?.emailContactDataEntries?.hasEmailNotificationDelivery ? "1" : "0") : missingInput(`dataIn.employeeExtension.emailContactDataEntries.hasEmailNotificationDelivery`, "bool", dataIn?.employeeExtension?.emailContactDataEntries?.hasEmailNotificationDelivery));
+  dataOut += (dataIn?.employeeExtension?.emailContactDataEntries?.hasEmailNotificationDelivery !== undefined ? (dataIn?.employeeExtension?.emailContactDataEntries?.hasEmailNotificationDelivery ? "1" : "0") : "0");
   dataOut += dataIn?.employeeExtension?.employmentStatuses?.effectiveDate?.substring(0, 10)?.padEnd(10) ?? "".substring(0, 10).padEnd(10);
   dataOut += dataIn?.employeeExtension?.employmentStatuses?.employmentStatus?.substring(0, 6)?.padEnd(6) ?? "".substring(0, 6).padEnd(6);
   dataOut += dataIn?.employeeExtension?.employmentStatuses?.employmentStatusTypeId?.toFixed(0)?.substring(0, 3)?.padEnd(3) ?? "0".substring(0, 3).padEnd(3);
   dataOut += dataIn?.employeeExtension?.employmentStatuses?.expirationDate?.substring(0, 10)?.padEnd(10) ?? "".substring(0, 10).padEnd(10);
   dataOut += dataIn?.employeeExtension?.employmentStatusesForExtensionSnapshotDate?.substring(0, 26)?.padEnd(26) ?? "".substring(0, 26).padEnd(26);
-  dataOut += (dataIn?.employeeExtension?.faceRequiredFlag !== undefined ? (dataIn?.employeeExtension?.faceRequiredFlag ? "1" : "0") : missingInput(`dataIn.employeeExtension.faceRequiredFlag`, "bool", dataIn?.employeeExtension?.faceRequiredFlag));
-  dataOut += (dataIn?.employeeExtension?.fingerRequiredFlag !== undefined ? (dataIn?.employeeExtension?.fingerRequiredFlag ? "1" : "0") : missingInput(`dataIn.employeeExtension.fingerRequiredFlag`, "bool", dataIn?.employeeExtension?.fingerRequiredFlag));
+  dataOut += (dataIn?.employeeExtension?.faceRequiredFlag !== undefined ? (dataIn?.employeeExtension?.faceRequiredFlag ? "1" : "0") : "0");
+  dataOut += (dataIn?.employeeExtension?.fingerRequiredFlag !== undefined ? (dataIn?.employeeExtension?.fingerRequiredFlag ? "1" : "0") : "0");
   dataOut += dataIn?.employeeExtension?.firstName?.substring(0, 7)?.padEnd(7) ?? "".substring(0, 7).padEnd(7);
   dataOut += dataIn?.employeeExtension?.fullName?.substring(0, 16)?.padEnd(16) ?? "".substring(0, 16).padEnd(16);
-  dataOut += (dataIn?.employeeExtension?.hasKmailNotificationDelivery !== undefined ? (dataIn?.employeeExtension?.hasKmailNotificationDelivery ? "1" : "0") : missingInput(`dataIn.employeeExtension.hasKmailNotificationDelivery`, "bool", dataIn?.employeeExtension?.hasKmailNotificationDelivery));
+  dataOut += (dataIn?.employeeExtension?.hasKmailNotificationDelivery !== undefined ? (dataIn?.employeeExtension?.hasKmailNotificationDelivery ? "1" : "0") : "0");
   dataOut += dataIn?.employeeExtension?.hireDate?.substring(0, 10)?.padEnd(10) ?? "".substring(0, 10).padEnd(10);
   dataOut += dataIn?.employeeExtension?.identifier?.identifier?.substring(0, 18)?.padEnd(18) ?? "".substring(0, 18).padEnd(18);
-  dataOut += (dataIn?.employeeExtension?.isEmployee !== undefined ? (dataIn?.employeeExtension?.isEmployee ? "1" : "0") : missingInput(`dataIn.employeeExtension.isEmployee`, "bool", dataIn?.employeeExtension?.isEmployee));
-  dataOut += (dataIn?.employeeExtension?.isManager !== undefined ? (dataIn?.employeeExtension?.isManager ? "1" : "0") : missingInput(`dataIn.employeeExtension.isManager`, "bool", dataIn?.employeeExtension?.isManager));
+  dataOut += (dataIn?.employeeExtension?.isEmployee !== undefined ? (dataIn?.employeeExtension?.isEmployee ? "1" : "0") : "0");
+  dataOut += (dataIn?.employeeExtension?.isManager !== undefined ? (dataIn?.employeeExtension?.isManager ? "1" : "0") : "0");
   dataOut += dataIn?.employeeExtension?.jobAssigmentId?.toFixed(0)?.substring(0, 6)?.padEnd(6) ?? "0".substring(0, 6).padEnd(6);
   dataOut += dataIn?.employeeExtension?.jobAssignmentId?.toFixed(0)?.substring(0, 6)?.padEnd(6) ?? "0".substring(0, 6).padEnd(6);
   dataOut += dataIn?.employeeExtension?.lastName?.substring(0, 5)?.padEnd(5) ?? "".substring(0, 5).padEnd(5);
   dataOut += dataIn?.employeeExtension?.learnPath?.substring(0, 10)?.padEnd(10) ?? "".substring(0, 10).padEnd(10);
   for (let j: number = 0; j < 8; ++j) {
-  dataOut += (dataIn?.employeeExtension?.licenseTypeList[j]?.active !== undefined ? (dataIn?.employeeExtension?.licenseTypeList[j]?.active ? "1" : "0") : missingInput(`dataIn.employeeExtension.licenseTypeList[${j}].active`, "bool", dataIn?.employeeExtension?.licenseTypeList[j]?.active));
+  dataOut += (dataIn?.employeeExtension?.licenseTypeList[j]?.active !== undefined ? (dataIn?.employeeExtension?.licenseTypeList[j]?.active ? "1" : "0") : "0");
   dataOut += dataIn?.employeeExtension?.licenseTypeList[j]?.licenseId?.toFixed(0)?.substring(0, 3)?.padEnd(3) ?? "0".substring(0, 3).padEnd(3);
   dataOut += dataIn?.employeeExtension?.licenseTypeList[j]?.licenseType?.substring(0, 8)?.padEnd(8) ?? "".substring(0, 8).padEnd(8);
   dataOut += dataIn?.employeeExtension?.licenseTypeList[j]?.licenseTypeName?.substring(0, 31)?.padEnd(31) ?? "".substring(0, 31).padEnd(31);
@@ -768,10 +787,10 @@ export function convertObjectTopersonResp(dataIn: personResp): string {
   dataOut += dataIn?.employeeExtension?.lockoutResetDateTime?.substring(0, 19)?.padEnd(19) ?? "".substring(0, 19).padEnd(19);
   dataOut += dataIn?.employeeExtension?.logonProfile?.substring(0, 7)?.padEnd(7) ?? "".substring(0, 7).padEnd(7);
   dataOut += dataIn?.employeeExtension?.logonProfileId?.toFixed(0)?.substring(0, 3)?.padEnd(3) ?? "0".substring(0, 3).padEnd(3);
-  dataOut += (dataIn?.employeeExtension?.mfaRequired !== undefined ? (dataIn?.employeeExtension?.mfaRequired ? "1" : "0") : missingInput(`dataIn.employeeExtension.mfaRequired`, "bool", dataIn?.employeeExtension?.mfaRequired));
+  dataOut += (dataIn?.employeeExtension?.mfaRequired !== undefined ? (dataIn?.employeeExtension?.mfaRequired ? "1" : "0") : "0");
   dataOut += dataIn?.employeeExtension?.middleName?.substring(0, 1)?.padEnd(1) ?? "".substring(0, 1).padEnd(1);
   dataOut += dataIn?.employeeExtension?.notificationProfile?.substring(0, 25)?.padEnd(25) ?? "".substring(0, 25).padEnd(25);
-  dataOut += (dataIn?.employeeExtension?.passwordUpdateRequired !== undefined ? (dataIn?.employeeExtension?.passwordUpdateRequired ? "1" : "0") : missingInput(`dataIn.employeeExtension.passwordUpdateRequired`, "bool", dataIn?.employeeExtension?.passwordUpdateRequired));
+  dataOut += (dataIn?.employeeExtension?.passwordUpdateRequired !== undefined ? (dataIn?.employeeExtension?.passwordUpdateRequired ? "1" : "0") : "0");
   dataOut += dataIn?.employeeExtension?.passwordUpdatedDateTime?.substring(0, 19)?.padEnd(19) ?? "".substring(0, 19).padEnd(19);
   for (let j: number = 0; j < 4; ++j) {
   dataOut += dataIn?.employeeExtension?.personCustomDataEntries[j]?.customDataType?.substring(0, 10)?.padEnd(10) ?? "".substring(0, 10).padEnd(10);
@@ -797,12 +816,12 @@ export function convertObjectTopersonResp(dataIn: personResp): string {
   dataOut += dataIn?.employeeExtension?.preferenceProfileDataEntry?.preferenceProfile?.substring(0, 16)?.padEnd(16) ?? "".substring(0, 16).padEnd(16);
   dataOut += dataIn?.employeeExtension?.preferenceProfileDataEntry?.preferenceProfileId?.toFixed(0)?.substring(0, 5)?.padEnd(5) ?? "0".substring(0, 5).padEnd(5);
   dataOut += dataIn?.employeeExtension?.preferenceProfileDataEntry?.schedulePeriodId?.toFixed(0)?.substring(0, 3)?.padEnd(3) ?? "0".substring(0, 3).padEnd(3);
-  dataOut += (dataIn?.employeeExtension?.preferenceProfileDataEntry?.usingShiftLabel !== undefined ? (dataIn?.employeeExtension?.preferenceProfileDataEntry?.usingShiftLabel ? "1" : "0") : missingInput(`dataIn.employeeExtension.preferenceProfileDataEntry.usingShiftLabel`, "bool", dataIn?.employeeExtension?.preferenceProfileDataEntry?.usingShiftLabel));
-  dataOut += (dataIn?.employeeExtension?.preferenceProfileDataEntry?.usesTwelveHourFormat !== undefined ? (dataIn?.employeeExtension?.preferenceProfileDataEntry?.usesTwelveHourFormat ? "1" : "0") : missingInput(`dataIn.employeeExtension.preferenceProfileDataEntry.usesTwelveHourFormat`, "bool", dataIn?.employeeExtension?.preferenceProfileDataEntry?.usesTwelveHourFormat));
+  dataOut += (dataIn?.employeeExtension?.preferenceProfileDataEntry?.usingShiftLabel !== undefined ? (dataIn?.employeeExtension?.preferenceProfileDataEntry?.usingShiftLabel ? "1" : "0") : "0");
+  dataOut += (dataIn?.employeeExtension?.preferenceProfileDataEntry?.usesTwelveHourFormat !== undefined ? (dataIn?.employeeExtension?.preferenceProfileDataEntry?.usesTwelveHourFormat ? "1" : "0") : "0");
   dataOut += dataIn?.employeeExtension?.processEmployeeProfile?.substring(0, 13)?.padEnd(13) ?? "".substring(0, 13).padEnd(13);
   dataOut += dataIn?.employeeExtension?.processManagerProfile?.substring(0, 13)?.padEnd(13) ?? "".substring(0, 13).padEnd(13);
-  dataOut += (dataIn?.employeeExtension?.requirePassChange !== undefined ? (dataIn?.employeeExtension?.requirePassChange ? "1" : "0") : missingInput(`dataIn.employeeExtension.requirePassChange`, "bool", dataIn?.employeeExtension?.requirePassChange));
-  dataOut += (dataIn?.employeeExtension?.restrictedUser !== undefined ? (dataIn?.employeeExtension?.restrictedUser ? "1" : "0") : missingInput(`dataIn.employeeExtension.restrictedUser`, "bool", dataIn?.employeeExtension?.restrictedUser));
+  dataOut += (dataIn?.employeeExtension?.requirePassChange !== undefined ? (dataIn?.employeeExtension?.requirePassChange ? "1" : "0") : "0");
+  dataOut += (dataIn?.employeeExtension?.restrictedUser !== undefined ? (dataIn?.employeeExtension?.restrictedUser ? "1" : "0") : "0");
   dataOut += dataIn?.employeeExtension?.seniorityDate?.substring(0, 10)?.padEnd(10) ?? "".substring(0, 10).padEnd(10);
   dataOut += dataIn?.employeeExtension?.shortName?.substring(0, 7)?.padEnd(7) ?? "".substring(0, 7).padEnd(7);
   dataOut += dataIn?.employeeExtension?.supervisorFullName?.substring(0, 15)?.padEnd(15) ?? "".substring(0, 15).padEnd(15);
@@ -812,13 +831,13 @@ export function convertObjectTopersonResp(dataIn: personResp): string {
   dataOut += dataIn?.employeeExtension?.telContactDataEntries[j]?.contactData?.substring(0, 14)?.padEnd(14) ?? "".substring(0, 14).padEnd(14);
   dataOut += dataIn?.employeeExtension?.telContactDataEntries[j]?.contactType?.substring(0, 7)?.padEnd(7) ?? "".substring(0, 7).padEnd(7);
   dataOut += dataIn?.employeeExtension?.telContactDataEntries[j]?.contactTypeId?.toFixed(0)?.substring(0, 3)?.padEnd(3) ?? "0".substring(0, 3).padEnd(3);
-  dataOut += (dataIn?.employeeExtension?.telContactDataEntries[j]?.smsswitch !== undefined ? (dataIn?.employeeExtension?.telContactDataEntries[j]?.smsswitch ? "1" : "0") : missingInput(`dataIn.employeeExtension.telContactDataEntries[${j}].smsswitch`, "bool", dataIn?.employeeExtension?.telContactDataEntries[j]?.smsswitch));
+  dataOut += (dataIn?.employeeExtension?.telContactDataEntries[j]?.smsswitch !== undefined ? (dataIn?.employeeExtension?.telContactDataEntries[j]?.smsswitch ? "1" : "0") : "0");
   }
   dataOut += dataIn?.employeeExtension?.timeZone?.substring(0, 25)?.padEnd(25) ?? "".substring(0, 25).padEnd(25);
   dataOut += dataIn?.employeeExtension?.timeZoneId?.toFixed(0)?.substring(0, 7)?.padEnd(7) ?? "0".substring(0, 7).padEnd(7);
   dataOut += dataIn?.employeeExtension?.userAccountId?.toFixed(0)?.substring(0, 6)?.padEnd(6) ?? "0".substring(0, 6).padEnd(6);
   dataOut += dataIn?.employeeExtension?.userName?.substring(0, 24)?.padEnd(24) ?? "".substring(0, 24).padEnd(24);
-  dataOut += (dataIn?.exceptionPresent !== undefined ? (dataIn?.exceptionPresent ? "1" : "0") : missingInput(`dataIn.exceptionPresent`, "bool", dataIn?.exceptionPresent));
+  dataOut += (dataIn?.exceptionPresent !== undefined ? (dataIn?.exceptionPresent ? "1" : "0") : "0");
 
   return dataOut;
 }
