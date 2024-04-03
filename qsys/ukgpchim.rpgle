3d334e1bfae457d43ffa497b0479f06e0876153b
mike corbo <mike.corbo@nrsonline.com>
2024-04-03 17:38:39 +0000

    UKG punch upload

---
+++ qsys/ukgpchim.rpgle
@@ -0,0 +1,50 @@
+     H Nomain
+
+      ************************************
+      * Module: ukgpchim
+      * Generated RPG conversion procedures
+      * Do not modify
+      ************************************
+
+      /include ukgpchim_h
+
+      ************************************
+      * Global variables
+      ************************************
+     DBufPtr           S               *
+     DCharBuf          S          64512A   Based(BufPtr)
+     Di                S             10U 0
+     Dj                S             10U 0
+     Dk                S             10U 0
+     Dl                S             10U 0
+
+      ************************************
+      * Convert PunchReqDS to buffer
+      ************************************
+     PPunchReqDSToBuf  B                   Export
+
+     DPunchReqDSToBuf  PI
+     DDataStruct                           LikeDS(PunchReqDS)
+     D                                     Const
+     DBuffer                         78A
+
+      * Initialize to beginning of buffer
+       BufPtr = %addr(Buffer);
+
+      * Write fields from DS to buffer
+       for i = 1 to 1;
+       %subst(CharBuf:1:8) = %char(DataStruct.punches(i).employee.id);
+       BufPtr += 8;
+       %subst(CharBuf:1:12) = %char(DataStruct.punches(i).punchGeoLocation.latitude);
+       BufPtr += 12;
+       %subst(CharBuf:1:12) = %char(DataStruct.punches(i).punchGeoLocation.longitude);
+       BufPtr += 12;
+       %subst(CharBuf:1:23) = DataStruct.punches(i).enteredOnDtm;
+       BufPtr += 23;
+       %subst(CharBuf:1:23) = DataStruct.punches(i).punchDtm;
+       BufPtr += 23;
+       endfor;
+
+       return ;
+
+     PPunchReqDSToBuf  E
---
+++ src/interfaces/ukgpchim.ts
@@ -0,0 +1,114 @@
+/* eslint-disable */
+// @ts-nocheck
+// Module: ukgpchim
+// Generated source -- do not modify
+
+import { ibmiConversions, missingInput } from "@eradani-inc/ec-client";
+const {
+  fromIbmiDate,
+  fromIbmiTime,
+  fromIbmiTimestamp,
+  toIbmiDate,
+  toIbmiTime,
+  toIbmiTimestamp
+} = ibmiConversions;
+
+/**
+ * Output interface
+ */
+export interface employeeDS {
+    /**
+     * @size 6 digits
+     * @precision 0 decimals
+     * @default `0`
+     */
+    id: number
+}
+
+/**
+ * Output interface
+ */
+export interface punchGeoLocationDS {
+    /**
+     * @size 10 digits
+     * @precision 6 decimals
+     * @default `0.0`
+     */
+    latitude: number,
+    /**
+     * @size 10 digits
+     * @precision 6 decimals
+     * @default `0.0`
+     */
+    longitude: number
+}
+
+/**
+ * Output interface
+ */
+export interface punchesDS {
+    /**
+     */
+    employee: employeeDS,
+    /**
+     */
+    punchGeoLocation: punchGeoLocationDS,
+    /**
+     * @size 23 characters
+     * @default `''`
+     */
+    enteredOnDtm: string,
+    /**
+     * @size 23 characters
+     * @default `''`
+     */
+    punchDtm: string
+}
+
+/**
+ * Output interface
+ */
+export interface PunchReqDS {
+    /**
+     */
+    punches: Array<punchesDS>
+}
+
+/**
+ * Convert PunchReqDS record to TypeScript object
+ */
+export function convertPunchReqDSToObject(dataIn: string): PunchReqDS {
+  const dataOut: any =   {
+  
+    };
+  let pos: number = 0;
+
+  dataOut.punches = [
+    
+  ];
+  for (let i: number = 0; i < 1; ++i) {
+  dataOut.punches[i] =   {
+  
+    };
+  dataOut.punches[i].employee =   {
+  
+    };
+  dataOut.punches[i].employee.id = Number(dataIn.substring(pos, pos + 8).trimEnd());
+  pos += 8;
+  dataOut.punches[i].punchGeoLocation =   {
+  
+    };
+  dataOut.punches[i].punchGeoLocation.latitude = Number(dataIn.substring(pos, pos + 12).trimEnd());
+  pos += 12;
+  dataOut.punches[i].punchGeoLocation.longitude = Number(dataIn.substring(pos, pos + 12).trimEnd());
+  pos += 12;
+  dataOut.punches[i].enteredOnDtm = dataIn.substring(pos, pos + 23).trimEnd();
+  pos += 23;
+  dataOut.punches[i].punchDtm = dataIn.substring(pos, pos + 23).trimEnd();
+  pos += 23;
+  }
+
+  return dataOut;
+}
+
+/* eslint-enable */
