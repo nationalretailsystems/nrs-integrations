a1ba2fb2dbb8a85ada0560ea7c4f2645e41b7843
mike corbo <mike.corbo@nrsonline.com>
2024-11-13 19:19:20 +0000

    dray dog fix

--- src/commands/draydog/controller.ts
+++ src/commands/draydog/controller.ts
@@ -105,6 +105,60 @@ export const getDrayDogCT: ECCHandlerFunction = async (reqkey, data, ecc) => {
             params: {
                 'page_size': '100',
                 'container_cycle_state': 'import_appt_booking',
+            }
+        });
+    } catch (err) {
+        if (err.response) {
+            // If the request was made and the server responded with a status code
+            // That falls out of the range of 2xx
+            // Note: These error formats are dependent on the web service
+            return ecc.sendEccResult('ECC8100', err.response.status + '-' + err.response.statusText, nextReqKey);
+        }
+
+        // Else the request was made but no response was received
+        // Note: This error format has nothing to do with the web service. This is
+        // Mainly TCP/IP errors.
+        return ecc.sendEccResult('ECC9100', err.message, nextReqKey);
+    }
+
+    // try {
+    //     await fs.writeFile(reqFields.filename, JSON.stringify(result.data), 'utf-8');
+    // } catch (err) {
+    //     return ecc.sendEccResult('ECC9200', err.message, nextReqKey);
+    // }
+
+    // Send the result info
+    try {
+        // let responseData = sanitizeValues(result.data, safeValues);
+        let responseData = result.data;
+        logger.debug('ECC0000', 'Success', nextReqKey);
+        nextReqKey = await ecc.sendEccResult('ECC0000', 'Success', nextReqKey);
+        nextReqKey = await ecc.sendObjectsToCaller(responseData, ddgetctcvt.convertObjectToResDDGet, nextReqKey);
+        logger.debug('Sent data to RPG');
+        return nextReqKey;
+    } catch (err) {
+        logger.error('Call failed', err);
+        return ecc.sendEccResult('ECC9300', err.message, nextReqKey);
+    }
+};
+export const getDrayDog1CT: ECCHandlerFunction = async (reqkey, data, ecc) => {
+    logger.debug(`Received draydog get contaner request`, { reqkey, data });
+    // Get parameters from incoming data buffer
+    // No reqfields on this api
+    const reqFields = ddgetctcvt.convertReqDDGetToObject(data);
+    // Call web service
+    let result;
+    let nextReqKey = reqkey;
+
+    try {
+        result = await axiosInstance.get('/containers/', {
+            headers: {
+                accept: 'application/json',
+                Authorization: draydog.apikey
+            },
+            params: {
+                'page_size': '100',
+                'container_cycle_state': 'import_appt_booking',
                 'container_numbers': reqFields.container
             }
         });
--- src/commands/draydog/index.ts
+++ src/commands/draydog/index.ts
@@ -1,9 +1,11 @@
 import { ECCInternalRouter } from '@eradani-inc/ecc-router/ecc-router';
-import { putDrayDogCT, getDrayDogCT } from './controller';
+import { putDrayDogCT, getDrayDogCT, getDrayDog1CT} from './controller';
 import * as draydogget from 'src/interfaces/ddgetct';
 import * as draydogput from 'src/interfaces/ddputct';
 
+
 export default function registerManagerplus(router: ECCInternalRouter) {
     router.use('getDrayDogCT', draydogget, getDrayDogCT);
+    router.use('getDrayDog1CT', draydogget, getDrayDog1CT);
     router.use('putDrayDogCT', draydogput, putDrayDogCT);
 }
